dd0c63e0f6e4c304ca9decfed4324cce8f4ad611
Brian Bolt <brian@mcneilco.com>
2013-05-24 23:53:33 +0000

    configuration file update

--- public/src/conf/configurationNode.js
+++ public/src/conf/configurationNode.js
@@ -4,20 +4,19 @@
 
 		exports.serverConfigurationParams = {};
 		exports.serverConfigurationParams.configuration = {};
-		exports.serverConfigurationParams.configuration.serverName = "http://localhost";
-		exports.serverConfigurationParams.configuration.portNumber = 3000;
-		exports.serverConfigurationParams.configuration.fileServiceURL = "http://localhost:8888";
-		exports.serverConfigurationParams.configuration.serverAddress = "ora.labsynch.com";
-		exports.serverConfigurationParams.configuration.driver = "oracle.jdbc.driver.OracleDriver";
-		exports.serverConfigurationParams.configuration.driverLocation = "public/src/modules/GenericDataParser/src/server/ojdbc6.jar";
-		exports.serverConfigurationParams.configuration.databaseLocation = "jdbc:oracle:thin:@";
-		exports.serverConfigurationParams.configuration.databasePort = ":1521:osl";
-		exports.serverConfigurationParams.configuration.username = "acas_dev";
-		exports.serverConfigurationParams.configuration.password = "acas_dev_password";
-		exports.serverConfigurationParams.configuration.serverPath = "http://suse.labsynch.com:8080/acas/";
-
+		exports.serverConfigurationParams.configuration.serverName = "http://host3.labsynch.com"
+		exports.serverConfigurationParams.configuration.portNumber = 3000
+		exports.serverConfigurationParams.configuration.fileServiceURL = "http://host3.labsynch.com:8888"
+		exports.serverConfigurationParams.configuration.serverAddress = "host3.labsynch.com"
+		exports.serverConfigurationParams.configuration.driver = "org.postgresql.Driver"
+		exports.serverConfigurationParams.configuration.driverLocation = "public/src/modules/GenericDataParser/src/server/postgresql-9.1-901.jdbc3.jar"
+		exports.serverConfigurationParams.configuration.databaseLocation = "jdbc:postgresql://"
+		exports.serverConfigurationParams.configuration.databasePort = ":5432/compound"
+		exports.serverConfigurationParams.configuration.username = "labseer"
+		exports.serverConfigurationParams.configuration.password = "labseer"
+		exports.serverConfigurationParams.configuration.serverPath = "http://host3.labsynch.com:8080/acas/"
 // For preferred ID service
-		exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://localhost:3000/api/preferredBatchId";
+		exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://host3.labsynch.com:3000/api/preferredBatchId"
 		exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceType = "LabSynchCmpdReg";
 		exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceURL = "http://host3.labsynch.com:8080/cmpdreg/metalots/corpName/";
 //		exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceType = "SingleBatchNameQueryString";
@@ -33,18 +32,18 @@
 
 // For racas
 		exports.serverConfigurationParams.configuration.appName = "ACAS";
-		exports.serverConfigurationParams.configuration.db_driver = "JDBC('oracle.jdbc.driver.OracleDriver', 'public/src/modules/GenericDataParser/src/server/ojdbc6.jar')";
-		exports.serverConfigurationParams.configuration.db_user = "acas_dev";
-		exports.serverConfigurationParams.configuration.db_password = "acas_dev_password";
-		exports.serverConfigurationParams.configuration.db_name = "osl";
-		exports.serverConfigurationParams.configuration.db_host = "ora.labsynch.com";
-		exports.serverConfigurationParams.configuration.db_port = "1521";
+		exports.serverConfigurationParams.configuration.db_driver = "PostgreSQL()";
+		exports.serverConfigurationParams.configuration.db_user = "labseer";
+		exports.serverConfigurationParams.configuration.db_password = "labseer";
+		exports.serverConfigurationParams.configuration.db_name = "compound";
+		exports.serverConfigurationParams.configuration.db_host = "host3.labsynch.com";
+		exports.serverConfigurationParams.configuration.db_port = "5432";
 		exports.serverConfigurationParams.configuration.stringsAsFactors = "FALSE";
-		exports.serverConfigurationParams.configuration.db_driver_package = "require(RJDBC)";
+		exports.serverConfigurationParams.configuration.db_driver_package = "require(RPostgreSQL)";
 
 // For R curve curation
-		exports.serverConfigurationParams.configuration.rapache = "http://suse.labsynch.com/r-services-api";
-		exports.serverConfigurationParams.configuration.rshiny = "http://suse.labsynch.com:3838"
+		exports.serverConfigurationParams.configuration.rapache = "http://host3.labsynch.com/r-services-api";
+		exports.serverConfigurationParams.configuration.rshiny = "http://host3.labsynch.com:3838"
 
 	})((typeof process === "undefined" || !process.versions ? window.configurationNode = window.configurationNode || {} : exports));
 
