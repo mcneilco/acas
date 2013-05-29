c92db7d67602bf9639034c312d710d6905a1ed52
Brian Bolt <brian@mcneilco.com>
2013-05-29 14:01:22 -0700

    dns configuration

--- public/src/conf/configuration.js
+++ public/src/conf/configuration.js
@@ -1,8 +1,8 @@
 window.SeuratAddOns = {}
 SeuratAddOns.configuration = {}
-SeuratAddOns.configuration.serverName = "http://acas-d"
+SeuratAddOns.configuration.serverName = "http://acas-d.dart.corp"
 SeuratAddOns.configuration.portNumber = 3000
-SeuratAddOns.configuration.fileServiceURL = "http://acas-d:8888"
+SeuratAddOns.configuration.fileServiceURL = "http://acas-d.dart.corp:8888"
 SeuratAddOns.configuration.serverAddress = "***REMOVED***"
 SeuratAddOns.configuration.driver = "oracle.jdbc.driver.OracleDriver"
 SeuratAddOns.configuration.driverLocation = "public/src/modules/GenericDataParser/src/server/ojdbc6.jar"
@@ -10,8 +10,8 @@ SeuratAddOns.configuration.databaseLocation = "jdbc:oracle:thin:@"
 SeuratAddOns.configuration.databasePort = ":1521:oradev"
 SeuratAddOns.configuration.username = "ACAS"
 SeuratAddOns.configuration.password = "2Ydudu8$pD"
-SeuratAddOns.configuration.serverPath = "http://acas-d:8080/acas/"
-SeuratAddOns.configuration.preferredBatchIdService = "http://acas-d:3000/api/preferredBatchId"
+SeuratAddOns.configuration.serverPath = "http://acas-d.dart.corp:8080/acas/"
+SeuratAddOns.configuration.preferredBatchIdService = "http://acas-d.dart.corp:3000/api/preferredBatchId"
 //For racas
 SeuratAddOns.configuration.databaseName = "ORADEV"
 SeuratAddOns.configuration.databasePortNumber = "1521"
