63751a498f4a9ef886f10a4a04cd485835c84be2
Brian Bolt <brian@mcneilco.com>
2013-05-29 18:30:00 -0700

    fixing config files

--- public/src/conf/configurationNode.js
+++ public/src/conf/configurationNode.js
@@ -4,19 +4,19 @@
 
                 exports.serverConfigurationParams = {};
                 exports.serverConfigurationParams.configuration = {};
-                exports.serverConfigurationParams.configuration.serverName = "http://acas-d.dart.corp"
+                exports.serverConfigurationParams.configuration.serverName = "http://acas-d"
                 exports.serverConfigurationParams.configuration.portNumber = 3000
-                exports.serverConfigurationParams.configuration.fileServiceURL = "http://acas-d.dart.corp:8888"
-                exports.serverConfigurationParams.configuration.serverAddress = "acas-d.dart.corp"
+                exports.serverConfigurationParams.configuration.fileServiceURL = "http://acas-d:8888"
+                exports.serverConfigurationParams.configuration.serverAddress = "acas-d"
                 exports.serverConfigurationParams.configuration.driver = "oracle.jdbc.driver.OracleDriver"
                 exports.serverConfigurationParams.configuration.driverLocation = "public/src/modules/GenericDataParser/src/server/ojdbc6.jar"
                 exports.serverConfigurationParams.configuration.databaseLocation = "jdbc:oracle:thin:@"
                 exports.serverConfigurationParams.configuration.databasePort = ":1521:oradev"
                 exports.serverConfigurationParams.configuration.username = "ACAS"
                 exports.serverConfigurationParams.configuration.password = "2Ydudu8$pD"
-                exports.serverConfigurationParams.configuration.serverPath = "http://acas-d.dart.corp:8080/acas/"
+                exports.serverConfigurationParams.configuration.serverPath = "http://acas-d:8080/acas/"
 // For preferred ID service
-                exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://acas-d.dart.corp:3000/api/preferredBatchId"
+                exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://acas-d:3000/api/preferredBatchId"
                 exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceType = "SingleBatchNameQueryString";
                 exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceURL = "http://dsanpimapp01:8080/DNS/core/v1/synonyms/preferred/";
 
@@ -37,8 +37,8 @@
                 exports.serverConfigurationParams.configuration.stringsAsFactors = "FALSE";
                 exports.serverConfigurationParams.configuration.db_driver_package = "require(ROracle)";
 // For R curve curation
-                exports.serverConfigurationParams.configuration.rapache = "http://acas-d.dart.corp/r-services-api";
-                exports.serverConfigurationParams.configuration.rshiny = "http://acas-d.dart.corp:3838"
+                exports.serverConfigurationParams.configuration.rapache = "http://acas-d/r-services-api";
+                exports.serverConfigurationParams.configuration.rshiny = "http://acas-d:3838"
 
         })((typeof process === "undefined" || !process.versions ? window.configurationNode = window.configurationNode || {} : exports));
 
