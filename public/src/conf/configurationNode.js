05506b5110e5c5e9eadf8983739d7015c5ae6670
Samuel Meyer <sam@mcneilco.com>
2013-06-24 14:47:17 -0700

    dns: changed name back acas-d

--- public/src/conf/configurationNode.js
+++ public/src/conf/configurationNode.js
@@ -4,34 +4,34 @@
 
                 exports.serverConfigurationParams = {};
                 exports.serverConfigurationParams.configuration = {};
-                exports.serverConfigurationParams.configuration.serverName = "http://acas-d.dart.corp"
-                exports.serverConfigurationParams.configuration.portNumber = 3000
-                exports.serverConfigurationParams.configuration.fileServiceURL = "http://acas-d.dart.corp:8888"
-		exports.serverConfigurationParams.configuration.serverRelativeFilePath = "serverOnlyModules/blueimp-file-upload-node/public/files/";
-                exports.serverConfigurationParams.configuration.serverAddress = "acas-d.dart.corp"
-                exports.serverConfigurationParams.configuration.driver = "oracle.jdbc.driver.OracleDriver"
-                exports.serverConfigurationParams.configuration.driverLocation = "public/src/modules/GenericDataParser/src/server/ojdbc6.jar"
-                exports.serverConfigurationParams.configuration.databaseLocation = "jdbc:oracle:thin:@"
-                exports.serverConfigurationParams.configuration.databasePort = ":1521:oradev"
-                exports.serverConfigurationParams.configuration.username = "ACAS"
-                exports.serverConfigurationParams.configuration.password = "2Ydudu8$pD"
-                exports.serverConfigurationParams.configuration.serverPath = "http://acas-d.dart.corp:8080/acas/"
+                exports.serverConfigurationParams.configuration.serverName = "http://acas-d";
+                exports.serverConfigurationParams.configuration.portNumber = 3000;
+                exports.serverConfigurationParams.configuration.fileServiceURL = "http://acas-d:8888";
+				exports.serverConfigurationParams.configuration.serverRelativeFilePath = "serverOnlyModules/blueimp-file-upload-node/public/files/";
+                exports.serverConfigurationParams.configuration.serverAddress = "acas-d";
+                exports.serverConfigurationParams.configuration.driver = "oracle.jdbc.driver.OracleDriver";
+                exports.serverConfigurationParams.configuration.driverLocation = "public/src/modules/GenericDataParser/src/server/ojdbc6.jar";
+                exports.serverConfigurationParams.configuration.databaseLocation = "jdbc:oracle:thin:@";
+                exports.serverConfigurationParams.configuration.databasePort = ":1521:oradev";
+                exports.serverConfigurationParams.configuration.username = "ACAS";
+                exports.serverConfigurationParams.configuration.password = "2Ydudu8$pD";
+                exports.serverConfigurationParams.configuration.serverPath = "http://acas-d:8080/acas/";
 // For preferred ID service
-                exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://acas-d.dart.corp:3000/api/preferredBatchId"
+                exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://acas-d:3000/api/preferredBatchId";
                 exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceType = "SingleBatchNameQueryString";
                 exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceURL = "http://dsanpimapp01:8080/DNS/core/v1/synonyms/preferred/";
 
 // For Login
 //                exports.serverConfigurationParams.configuration.userAuthenticationType = "Demo";
 //                exports.serverConfigurationParams.configuration.userAuthenticationServiceURL = "";
-              exports.serverConfigurationParams.configuration.userAuthenticationType = "DNS";
-              exports.serverConfigurationParams.configuration.userAuthenticationServiceURL = "http://imapp01:8080/DNS/persons/v1/Persons/authenticate";
+				exports.serverConfigurationParams.configuration.userAuthenticationType = "DNS";
+				exports.serverConfigurationParams.configuration.userAuthenticationServiceURL = "http://imapp01:8080/DNS/persons/v1/Persons/authenticate";
 
 // For Projects
 //		exports.serverConfigurationParams.configuration.projectsType = "ACAS";
 //		exports.serverConfigurationParams.configuration.projectsServiceURL = "http://tbd";
-		exports.serverConfigurationParams.configuration.projectsType = "DNS";
-		exports.serverConfigurationParams.configuration.projectsServiceURL = "http://dsanpimapp01:8080/DNS/core/v1/DNSCode/Project.json";
+				exports.serverConfigurationParams.configuration.projectsType = "DNS";
+				exports.serverConfigurationParams.configuration.projectsServiceURL = "http://dsanpimapp01:8080/DNS/core/v1/DNSCode/Project.json";
 
 // For racas
                 exports.serverConfigurationParams.configuration.appName = "ACAS";
@@ -43,20 +43,21 @@
                 exports.serverConfigurationParams.configuration.db_port = "1521";
                 exports.serverConfigurationParams.configuration.stringsAsFactors = "FALSE";
                 exports.serverConfigurationParams.configuration.db_driver_package = "require(ROracle)";
+
 // For R curve curation
-		exports.serverConfigurationParams.configuration.rapache = "http://acas-d.dart.corp/r-services-api";
-		exports.serverConfigurationParams.configuration.rshiny = "http://acas-d.dart.corp:3838"
+				exports.serverConfigurationParams.configuration.rapache = "http://acas-d/r-services-api";
+				exports.serverConfigurationParams.configuration.rshiny = "http://acas-d:3838";
 
 // For generic data parser
-		exports.serverConfigurationParams.configuration.projectService = "http://acas-d.dart.corp:3000/api/projects";
-		exports.serverConfigurationParams.configuration.fileServiceType = "DNS";
-//		exports.serverConfigurationParams.configuration.fileServiceType = "blueimp";
-		exports.serverConfigurationParams.configuration.externalFileService = "http://dsandimapp01:8080/DNS/core/v1/DNSFile";
-		exports.serverConfigurationParams.configuration.stateGroupsScript = "public/src/conf/genericDataParserConfiguration.R";
-		exports.serverConfigurationParams.configuration.includeProject = "TRUE";
-		exports.serverConfigurationParams.configuration.reportRegistrationURL = "http://dsandimapp01:8080/DNS/core/v1/DNSAnnotation";
-		exports.serverConfigurationParams.configuration.allowProtocolCreationWithFormats = ""
-
+				exports.serverConfigurationParams.configuration.projectService = "http://acas-d:3000/api/projects";
+		       exports.serverConfigurationParams.configuration.fileServiceType = "DNS";
+//		       exports.serverConfigurationParams.configuration.fileServiceType = "blueimp";
+		       exports.serverConfigurationParams.configuration.externalFileService = "http://dsandimapp01:8080/DNS/core/v1/DNSFile";
+		       exports.serverConfigurationParams.configuration.stateGroupsScript = "public/src/conf/genericDataParserConfiguration.R";
+		       exports.serverConfigurationParams.configuration.includeProject = "TRUE";
+		       exports.serverConfigurationParams.configuration.reportRegistrationURL = "http://dsandimapp01:8080/DNS/core/v1/DNSAnnotation";
+		       exports.serverConfigurationParams.configuration.allowProtocolCreationWithFormats = ""
+		       
         })((typeof process === "undefined" || !process.versions ? window.configurationNode = window.configurationNode || {} : exports));
 
 }).call(this);
