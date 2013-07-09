53f76394516bf47e0315d35646e759efd4f81b25
Brian Bolt <brian@mcneilco.com>
2013-07-09 11:33:28 -0700

    updated with better error handling and logging

--- public/src/modules/API_ANALYSIS_GROUP_RESULTS_UPDATER/src/API_ANALYSIS_GROUP_RESULTS_UPDATE.R
+++ public/src/modules/API_ANALYSIS_GROUP_RESULTS_UPDATER/src/API_ANALYSIS_GROUP_RESULTS_UPDATE.R
@@ -1,13 +1,14 @@
 #API_ANALYSIS_GROUP_RESULTS_UPDATER.R
 require(racas)
 require(logging)
+
 logReset()
 basicConfig(level='FINEST')
 logName <- "com.dartneuroscience.acas.api_analysis_group_results_updater"
-getLogger(logName)$addHandler(writeToFile, file="../../../../../../api_analysis_group_results_updater.log")
+logFilePath <- paste0(applicationSettings$logDir,"/api_analysis_group_results_updater.log")
+getLogger(logName)$addHandler(writeToFile, file=logFilePath)
 logger <- getLogger(logName)
-logger$info('API_ANALYSIS_GROUP_RESULTS Updater Initiated')
-  
+
 updaterApplicationSettings <- data.frame(
   db_driver = racas::applicationSettings$db_driver,
   db_user = "seurat",
@@ -23,17 +24,21 @@ from <- sprintf("<API_ANALYSIS_GROUP_RESULTS_UPDATER@%s>", Sys.info()[4])
 #to <- c("Brian-McNeilCo<brian@mcneilco.com>")
 to <- c()
 
-tryCatch(
-	{
+tryCatch({
+	logger$info('API_ANALYSIS_GROUP_RESULTS Updater Initiated')
 	#connect and build api_analysis_group_results table
 	conn <- getDatabaseConnection(applicationSettings = updaterApplicationSettings)
 	error_api_analysis_group_results <- FALSE
-	error_log <- c()
 
 	#drop _NEW table if exist
-	remove_tbl <- query("drop table SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	newTblRemoved <- query("drop table SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(newTblRemoved) == "list") {
+	  logger$error('Could not remove SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW')
+	  logger$error(newTblRemoved$error)
+	}
+	
 	#create new table _NEW
-	create_tbl <- query("CREATE table SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW as 
+	qu <- "CREATE table SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW as 
 								SELECT api_analysis_group_results.AG_ID,
     								api_analysis_group_results.EXPERIMENT_ID,
 								    api_analysis_group_results.TESTED_LOT,
@@ -53,105 +58,136 @@ tryCatch(
   									LEFT OUTER JOIN batch.batch
   									ON api_analysis_group_results.tested_lot=batch.corp_batch_name
   									LEFT OUTER JOIN batch.v_api_batch_alias
-  									ON api_analysis_group_results.tested_lot=v_api_batch_alias.alias",
-  									applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-  	
-  	if(create_tbl) {
-  		logger$info('API_ANALYSIS_GROUP_RESULTS_NEW Created')
-		#rename existing expt_id index on old table
- 		EXPERIMENT_ID <- query(" ALTER INDEX SEURAT.API_AGR_EXPT_ID_IDX1 RENAME TO API_AGR_EXPT_ID_IDX2 ",applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
- 		if(EXPERIMENT_ID) {
- 			#create new expt_id index on new table
- 			EXPERIMENT_ID <- query(" CREATE INDEX SEURAT.API_AGR_EXPT_ID_IDX1 ON SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW (EXPERIMENT_ID) NOLOGGING TABLESPACE KALYPSYSADMIN_NOLOG ", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
- 			if(EXPERIMENT_ID) {
- 				#rename existing sample_id index on old table
-				SAMPLE_ID <- query(" ALTER INDEX SEURAT.API_AGR_SAMPLE_ID_IDX1 RENAME TO API_AGR_SAMPLE_ID_IDX2 ", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-				if(SAMPLE_ID) {
-					#create new sample_id index on new table
-					SAMPLE_ID <- query(" CREATE INDEX SEURAT.API_AGR_SAMPLE_ID_IDX1 ON SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW (TESTED_LOT) NOLOGGING TABLESPACE KALYPSYSADMIN_NOLOG ", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-					if(SAMPLE_ID) {
-						#rename existing batch_id index on old table
-						BATCH_ID <- query(" ALTER INDEX SEURAT.API_AGR_BATCH_ID_IDX1 RENAME TO API_AGR_BATCH_ID_IDX2 ", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-						if(BATCH_ID) {
-							#create new batch_id index on new table
-							BATCH_ID <- query(" CREATE INDEX SEURAT.API_AGR_BATCH_ID_IDX1 ON SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW (BATCH_ID) NOLOGGING TABLESPACE KALYPSYSADMIN_NOLOG ", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-							if(BATCH_ID) {
-								logger$info('API_ANALYSIS_GROUP_RESULTS_NEW Indexes created')
-								#rename old table by adding _OLD
-								rename_tbl <- query(" ALTER table SEURAT.API_ANALYSIS_GROUP_RESULTS RENAME TO API_ANALYSIS_GROUP_RESULTS_OLD", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-								if(rename_tbl) {				
-									logger$info('API_ANALYSIS_GROUP_RESULTS Renamed to API_ANALYSIS_GROUP_RESULTS_OLD')
-									#rename new table by dropping the _NEW
-									rename_tbl <- query(" ALTER table SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW RENAME TO API_ANALYSIS_GROUP_RESULTS", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-									if(rename_tbl) {
-										logger$info('API_ANALYSIS_GROUP_RESULTS_NEW Renamed to API_ANALYSIS_GROUP_RESULTS')
-										#drop _OLD table
-										drop_tbl <- query(" DROP table SEURAT.API_ANALYSIS_GROUP_RESULTS_OLD ", applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
-										if(!drop_tbl) {
-											error_api_analysis_group_results <- TRUE
-											cat("drop old table error\n")
-										} else {
-											logger$info('Dropped API_ANALYSIS_GROUP_RESULTS_OLD')
-										}
-									} else {
-										error_api_analysis_group_results <- TRUE
-										logger$error("rename new table error\n")
-									}
-								} else {
-									error_api_analysis_group_results <- TRUE
-									logger$error("rename old table error\n")
-								}
-							} else {
-								error_api_analysis_group_results <- TRUE
-								logger$error("create new batch_id index error\n")
-							}
-						} else {
-							error_api_analysis_group_results <- TRUE
-							logger$error("rename batch_id index error\n")
-						}
-					} else {
-						error_api_analysis_group_results <- TRUE
-						logger$error("create new sample_id index error\n")
-					}
-				} else {
-					error_api_analysis_group_results <- TRUE
-					logger$error("rename old sample_id index error\n")
-				}
-			} else {
-				error_api_analysis_group_results <- TRUE
-				logger$error("create new expt_id index error\n")
-			}
-		} else {
-			error_api_analysis_group_results <- TRUE
-			logger$error("rename old expt_id index error\n")
-		}
+  									ON api_analysis_group_results.tested_lot=v_api_batch_alias.alias"
+	newTblCreated <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(newTblCreated) == "list") {
+	  logger$error(qu)
+	  logger$error(newTblCreated$error)
+	  error_api_analysis_group_results <- TRUE
 	} else {
-		error_api_analysis_group_results <- TRUE
-		logger$error("create new table error\n")
+	  logger$info(paste0(qu, " Successful"))
+	}
+
+	#rename existing expt_id index on old table
+	qu <- " ALTER INDEX SEURAT.API_AGR_EXPT_ID_IDX1 RENAME TO API_AGR_EXPT_ID_IDX2 "
+	experimentIDRenamed <- query(qu ,applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(experimentIDRenamed) == "list") {
+	  logger$error('Could not rename SEURAT.API_AGR_EXPT_ID_IDX1 to API_AGR_EXPT_ID_IDX2, creating from scratch')
+	  logger$error(experimentIDRenamed$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
 	}
 	
+	qu <- " CREATE INDEX SEURAT.API_AGR_EXPT_ID_IDX1 ON SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW (EXPERIMENT_ID) NOLOGGING TABLESPACE KALYPSYSADMIN_NOLOG "
+	experimentIDCreated <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(experimentIDCreated) == "list") {
+		logger$error(qu)
+	  	logger$error(experimentIDCreated$error)
+	  	error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
+	}
+	
+	#rename existing sample_id index on old table 	
+	qu <- " ALTER INDEX SEURAT.API_AGR_SAMPLE_ID_IDX1 RENAME TO API_AGR_SAMPLE_ID_IDX2 "
+	sampleIDRenamed <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(sampleIDRenamed) == "list") {
+	  logger$error(qu)
+	  logger$error(sampleIDRenamed$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
+	}
+	
+	#create new sample_id index on new table
+	qu <- " CREATE INDEX SEURAT.API_AGR_SAMPLE_ID_IDX1 ON SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW (TESTED_LOT) NOLOGGING TABLESPACE KALYPSYSADMIN_NOLOG "
+	sampleIDCreated <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(sampleIDCreated) == "list") {
+	  logger$error(qu)
+	  logger$error(sampleIDCreated$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
+	}
+	
+	#rename existing batch_id index on old table
+	qu <- " ALTER INDEX SEURAT.API_AGR_BATCH_ID_IDX1 RENAME TO API_AGR_BATCH_ID_IDX2 "
+	batchIDRenamed <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(batchIDRenamed) == "list") {
+	  logger$error(qu)
+	  logger$error(batchIDRenamed$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
+	}
+	
+	#create new batch_id index on new table
+	qu <- " CREATE INDEX SEURAT.API_AGR_BATCH_ID_IDX1 ON SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW (BATCH_ID) NOLOGGING TABLESPACE KALYPSYSADMIN_NOLOG"
+	batchIDCreated <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(batchIDCreated) == "list") {
+		logger$error(qu)
+	  	logger$error(batchIDCreated$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
+	}
+
+	#rename old table by adding _OLD
+	qu <- " ALTER table SEURAT.API_ANALYSIS_GROUP_RESULTS RENAME TO API_ANALYSIS_GROUP_RESULTS_OLD"
+	apiTableRenamedToOld <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(apiTableRenamedToOld) == "list") {
+		logger$error(qu)
+	  	logger$error(apiTableRenamedToOld$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
+	}
+	
+	#rename new table by dropping the _NEW	
+	qu <- " ALTER table SEURAT.API_ANALYSIS_GROUP_RESULTS_NEW RENAME TO API_ANALYSIS_GROUP_RESULTS"
+	newTableToAPIName <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(newTableToAPIName) == "list") {
+	  logger$error(qu)
+	  	logger$error(newTableToAPIName$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  logger$info(paste0(qu, " Successful"))
+	}
+	
+	#drop _OLD table
+	qu <- " DROP table SEURAT.API_ANALYSIS_GROUP_RESULTS_OLD"
+	dropOldTable <- query(qu, applicationSettings = updaterApplicationSettings, globalConnect = TRUE)
+	if(class(dropOldTable) == "list") {
+		logger$error(qu)
+	  	logger$error(dropOldTable$error)
+	  error_api_analysis_group_results <- TRUE
+	} else {
+	  	logger$info(paste0(qu, " Successful"))
+	}
+
 	if(error_api_analysis_group_results) {
 		rollback <- dbRollback(conn)
-		logger$error("API_ANALYSIS_GROUP_RESULTS update unsuccessful\n")
+		logger$error("API_ANALYSIS_GROUP_RESULTS update unsuccessful, rolledback ")
+		stop(paste0("API_ANALYSIS_GROUP_RESULTS update unsuccessful, rolled back\n for details see\n",logFilePath))
 	} else {
 		commited <- dbCommit(conn)
-		logger$error("API_ANALYSIS_GROUP_RESULTS successfully updated\n")
+		logger$info("API_ANALYSIS_GROUP_RESULTS successfully updated and committed")
 	}
 	disconnected <- dbDisconnect(conn)
-	},
-	error = function(ex) {
-		logger$error("  An error was detected.\n");
-		logger$error(paste("  Here is the error:\n",ex))
-		body <- paste("  Here is the error:\n",ex)
-		subject <- "Error performing API_ANALYSIS_GROUP_RESULTS Update"
-		send <- sendmail(from, to, subject, body)
-	}
-)
+},
+error = function(ex) {
+	logger$error(ex);
+	body <- paste("  Here is the error: ",ex)
+	subject <- "Error performing API_ANALYSIS_GROUP_RESULTS Update"
+	send <- sendmail(from, to, subject, body)
+	disconnected <- dbDisconnect(conn)
+})
 
 subject <- "Successful API_ANALYSIS_GROUP_RESULTS Update"
 body <- list("Updated API_ANALYSIS_GROUP_RESULTS Successfully")
 for(t in to) {
 	send <- sendmail(from, t, subject, body)
-	cat(paste("  Email sent to:",t,"\n"))
+	cat(paste("  Email sent to:",t," "))
 }
-logger$info("Finished API_ANALYSIS_GROUP_RESULTS\n")
+logger$info("Finished API_ANALYSIS_GROUP_RESULTS Successfully")
