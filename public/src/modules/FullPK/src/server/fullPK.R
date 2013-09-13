f64d53c3409f500bda569d424df5896bd53dad32
Samuel Meyer <sam@mcneilco.com>
2013-09-13 11:51:26 -0700

    PK: Added preprocessor error handling closing #87

--- public/src/modules/FullPK/src/server/fullPK.R
+++ public/src/modules/FullPK/src/server/fullPK.R
@@ -1,7 +1,5 @@
-source("public/src/modules/FullPK/src/server/fullPKPreprocessing.R")
-source("public/src/modules/GenericDataParser/src/server/generic_data_parser.R")
 
-# TODO: file annotation
+source("public/src/modules/GenericDataParser/src/server/generic_data_parser.R")
 
 parseFullPKData <- function(request){
   # Needs a list:
@@ -32,19 +30,47 @@ parseFullPKData <- function(request){
   inputParameters <- c(experimentMetaData = "", inputParameters)
   bioavailabilityIndex <- which(names(inputParameters)=="bioavailability")
   inputParameters <- c(inputParameters[1:bioavailabilityIndex - 1], list("", "rawData"=""), inputParameters[bioavailabilityIndex:length(inputParameters)])
+  preProcessorCall <- tryCatch({
+    source("public/src/modules/FullPK/src/server/fullPKPreprocessing.R")
+    response <- list(fileToParse = preprocessPK(request$fileToParse, inputParameters))
+    list(completedSuccessfully = TRUE, preProcessorResponse = response)
+  }, error = function(err) {
+    return(list(completedSuccessfully = FALSE, preProcessorResponse = err$message))			
+  })
   parserInput <- list(fileToParse = preprocessPK(request$fileToParse, inputParameters))
-  parserInput$dryRunMode <- request$dryRunMode
-  parserInput$reportFile <- request$reportFile
-  parserInput$user <- request$user
-  response <- parseGenericData(parserInput)
-  if (!interpretJSONBoolean(request$dryRunMode)) {
-    experiment <- fromJSON(getURL(paste0(racas::applicationSettings$serverPath, "experiments/codename/", response$results$experimentCode)))[[1]]
-    moveFileToExperimentFolder(request$fileToParse, experiment, request$user, response$transactionId, 
-                               racas::applicationSettings$fileServiceType, racas::applicationSettings$externalFileService)
+  
+  if(preProcessorCall$completedSuccessfully) {
+    parserInput <- preProcessorCall$preProcessorResponse
+    parserInput$dryRunMode <- request$dryRunMode
+    parserInput$user <- request$user
+    parserResponse <- parseGenericData(parserInput)
+    if (!interpretJSONBoolean(request$dryRunMode)) {
+      experiment <- fromJSON(getURL(paste0(racas::applicationSettings$serverPath, "experiments/codename/", parserResponse$results$experimentCode)))[[1]]
+      moveFileToExperimentFolder(request$fileToParse, experiment, request$user, response$transactionId, 
+                                 racas::applicationSettings$fileServiceType, racas::applicationSettings$externalFileService)
+    }
+    parserResponse$results <- c(parserResponse$results,preProcessorCall$preProcessorResponse)
+    return(parserResponse)
+  } else {
+    htmlSummary = paste0("<h4>The custom pre-processor encountered an error during execution</h4>",
+                         "<p>Please report the following error to your system administrator:</p>",
+                         "<p>",preProcessorCall$preProcessorResponse,"</p>")
+    response <- list(
+      commit= FALSE,
+      transactionId = -1,
+      results= list(
+        path= getwd(),
+        fileToParse= NULL,
+        dryRun= request$dryRunMode,
+        htmlSummary= htmlSummary
+      ),
+      hasError= TRUE,
+      hasWarning= FALSE,
+      errorMessages= list(list(errorLevel="error", message=preProcessorCall$preProcessorResponse))
+    )
+    return(response)
   }
-  return(response)
 }
-
 # Testing code
 #   #request <- list(user="smeyer", dryRunMode = "true", "fileToParse"="public/src/modules/FullPK/spec/specFiles/Worksheet.xls", reportFile="~/Desktop/new.txt")
 #   request <- list(user="smeyer", dryRunMode = "false", fileToParse="~/Documents/clients/DNS/PK/PK514v3.xls", reportFile="serverOnlyModules/blueimp-file-upload-node/public/files/Input v6.xls")
