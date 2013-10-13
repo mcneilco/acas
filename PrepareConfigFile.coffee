a39cec9fb082a2f0cdfb12d9c945183d472cf07f
John McNeil <john@mcneilco.com>
2013-10-13 09:33:40 -0700

    generate 2nd config file in alternate format for roo

--- PrepareConfigFile.coffee
+++ PrepareConfigFile.coffee
@@ -5,4 +5,14 @@ global.deployMode = "Dev"
 
 csUtilities.prepareConfigFile ->
 	csUtilities.logUsage("Configuration file generated", "deployMode: "+deployMode, "")
+	prepareRooConfig()
+
+prepareRooConfig = ->
+	config = require './public/src/conf/configurationNode.js'
+	fs = require('fs')
+
+	configOut = ""
+	for attr, value of config.serverConfigurationParams.configuration
+		configOut += attr+"="+value+"\n"
+	fs.writeFile "./public/src/conf/acas_roo.properties", configOut
 
--- PrepareConfigFile.js
+++ PrepareConfigFile.js
@@ -1,12 +1,27 @@
 (function() {
-  var csUtilities;
+  var csUtilities, prepareRooConfig;
 
   csUtilities = require("./public/src/conf/CustomerSpecificServerFunctions.js");
 
   global.deployMode = "Dev";
 
   csUtilities.prepareConfigFile(function() {
-    return csUtilities.logUsage("Configuration file generated", "deployMode: " + deployMode, "");
+    csUtilities.logUsage("Configuration file generated", "deployMode: " + deployMode, "");
+    return prepareRooConfig();
   });
+
+  prepareRooConfig = function() {
+    var attr, config, configOut, fs, value, _ref;
+
+    config = require('./public/src/conf/configurationNode.js');
+    fs = require('fs');
+    configOut = "";
+    _ref = config.serverConfigurationParams.configuration;
+    for (attr in _ref) {
+      value = _ref[attr];
+      configOut += attr + "=" + value + "\n";
+    }
+    return fs.writeFile("./public/src/conf/acas_roo.properties", configOut);
+  };
 
 }).call(this);
