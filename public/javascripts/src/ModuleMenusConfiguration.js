6dea0af60f78f8464c3da55c505591983179cb88
John McNeil <john@mcneilco.com>
2013-10-23 20:13:25 -0700

    Fixing capitalization problem with bulkload smaple transfers. Ticket #129

--- public/javascripts/src/ModuleMenusConfiguration.js
+++ public/javascripts/src/ModuleMenusConfiguration.js
@@ -7,6 +7,21 @@
       isHeader: false,
       menuName: "Load Experiment",
       mainControllerClassName: "GenericDataParserController"
+    }, {
+      isHeader: false,
+      menuName: "Analyze FLIPR Data",
+      mainControllerClassName: "PrimaryScreenExperimentController"
+    }, {
+      isHeader: true,
+      menuName: "Inventory"
+    }, {
+      isHeader: false,
+      menuName: "Load Containers From SDF",
+      mainControllerClassName: "BulkLoadContainersFromSDFController"
+    }, {
+      isHeader: false,
+      menuName: "Load Sample Transfer Log",
+      mainControllerClassName: "BulkLoadSampleTransfersController"
     }
   ];
 
