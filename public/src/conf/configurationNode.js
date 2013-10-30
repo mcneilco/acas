201b7c1b10642870acf6ecfdf624206bb70a1c6c
Samuel Meyer <sam@mcneilco.com>
2013-10-30 11:17:23 -0700

    suse: putting back correct config

--- public/src/conf/configurationNode.js
+++ public/src/conf/configurationNode.js
@@ -4,24 +4,24 @@
 
 		exports.serverConfigurationParams = {};
 		exports.serverConfigurationParams.configuration = {};
-		exports.serverConfigurationParams.configuration.serverName = "http://usubtrdapqv1";
-		exports.serverConfigurationParams.configuration.serverAddress = "usubtrdapqv1";
+		exports.serverConfigurationParams.configuration.serverName = "http://localhost";
+		exports.serverConfigurationParams.configuration.serverAddress = "localhost";
 		exports.serverConfigurationParams.configuration.portNumber = 3000;
-		exports.serverConfigurationParams.configuration.fileServiceURL = "http://usubtrdapqv1:8888";
+		exports.serverConfigurationParams.configuration.fileServiceURL = "http://localhost:8888";
 		exports.serverConfigurationParams.configuration.serverRelativeFilePath = "serverOnlyModules/blueimp-file-upload-node/public/files/";
 		exports.serverConfigurationParams.configuration.driver = "oracle.jdbc.driver.OracleDriver";
 		exports.serverConfigurationParams.configuration.driverLocation = "public/src/modules/GenericDataParser/src/server/ojdbc6.jar";
-		exports.serverConfigurationParams.configuration.databaseLocation = "jdbc:postgresql://";
+		exports.serverConfigurationParams.configuration.databaseLocation = "jdbc:oracle://";
 		exports.serverConfigurationParams.configuration.databasePort = ":5432/compound";
-		exports.serverConfigurationParams.configuration.username = "labseer";
-		exports.serverConfigurationParams.configuration.password = "labseer";
-		exports.serverConfigurationParams.configuration.serverPath = "http://usubtrdapqv1:8080/acas/";
+		exports.serverConfigurationParams.configuration.username = "acas_dev";
+		exports.serverConfigurationParams.configuration.password = "acas_dev_password";
+		exports.serverConfigurationParams.configuration.serverPath = "http://suse.labsynch.com:8080/acas/";
 		exports.serverConfigurationParams.configuration.enableSpecRunner = true;
-		exports.serverConfigurationParams.configuration.requireLogin = true;
+		exports.serverConfigurationParams.configuration.requireLogin = false;
 // For preferred ID service
-		exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://usubtrdapqv1:3000/api/preferredBatchId";
-		exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceType = "LabSynchCmpdReg";
-		exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceURL = "http:///usubtrdapqv1:8080/cmpdreg/metalots/corpName/";
+		exports.serverConfigurationParams.configuration.preferredBatchIdService = "http://localhost:3000/api/preferredBatchId";
+		exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceType = "SeuratCmpdReg";
+		exports.serverConfigurationParams.configuration.externalPreferredBatchIdServiceURL = "http://suse.labsynch.com:8080/cmpdreg/metalots/corpName/";
 
 // For Login
 		exports.serverConfigurationParams.configuration.userAuthenticationType = "Demo";
@@ -34,18 +34,18 @@
 
 // For racas
 		exports.serverConfigurationParams.configuration.appName = "ACAS";
-		exports.serverConfigurationParams.configuration.db_driver = "Oracle()";
-		exports.serverConfigurationParams.configuration.db_user = "acas";
-		exports.serverConfigurationParams.configuration.db_password = "acas_password";
-		exports.serverConfigurationParams.configuration.db_name = "trdq1.global.iff.com";
-		exports.serverConfigurationParams.configuration.db_host = "usubtrddbqv1.global.iff.com";
+		exports.serverConfigurationParams.configuration.db_driver = "JDBC('oracle.jdbc.driver.OracleDriver', 'public/src/modules/GenericDataParser/src/server/ojdbc6.jar')";
+		exports.serverConfigurationParams.configuration.db_user = "acas_dev";
+		exports.serverConfigurationParams.configuration.db_password = "acas_dev_password";
+		exports.serverConfigurationParams.configuration.db_name = "osl";
+		exports.serverConfigurationParams.configuration.db_host = "ora.labsynch.com";
 		exports.serverConfigurationParams.configuration.db_port = "1521";
 		exports.serverConfigurationParams.configuration.stringsAsFactors = "FALSE";
-		exports.serverConfigurationParams.configuration.db_driver_package = "require(ROracle)";
+		exports.serverConfigurationParams.configuration.db_driver_package = "require(RJDBC)";
 		exports.serverConfigurationParams.configuration.logDir = "/opt/node_apps/log";
 // For R curve curation
-		exports.serverConfigurationParams.configuration.rapache = "http://usubtrdapqv1/r-services-api";
-		exports.serverConfigurationParams.configuration.rshiny = "http://usubtrdapqv1:3838";
+		exports.serverConfigurationParams.configuration.rapache = "http://suse.labsynch.com/r-services-api";
+		exports.serverConfigurationParams.configuration.rshiny = "http://suse.labsynch.com:3838";
 
 // For generic data parser
 		exports.serverConfigurationParams.configuration.projectService = "";
@@ -55,8 +55,8 @@
 		exports.serverConfigurationParams.configuration.includeProject = "FALSE";
 		exports.serverConfigurationParams.configuration.reportRegistrationURL = "";
 		exports.serverConfigurationParams.configuration.allowProtocolCreationWithFormats = "Generic,Dose Response,Custom Example";
-		exports.serverConfigurationParams.configuration.nameValidationService = "http://usubtrdapqv1:3000/api/users";
-		exports.serverConfigurationParams.configuration.resultViewerProtocolPrefix = "http://usubtrdapqv1:9080/seurat/runseurat?cmd=newjob&AssayName=";
+		exports.serverConfigurationParams.configuration.nameValidationService = "http://localhost:3000/api/users";
+		exports.serverConfigurationParams.configuration.resultViewerProtocolPrefix = "http://suse.labsynch.com:9080/seurat/runseurat?cmd=newjob&AssayName=";
 		exports.serverConfigurationParams.configuration.resultViewerExperimentPrefix = "&AssayProtocol=";
 		exports.serverConfigurationParams.configuration.deleteFilesOnReload = "false";
 		exports.serverConfigurationParams.configuration.useCustomReportRegistration = "false"
