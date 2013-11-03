4986a180c7dff21a3cb262915499a0e5a32d134f
John McNeil <john@mcneilco.com>
2013-11-03 09:25:40 -0800

    Fixed customer specific functions to use new conf service (oversight)

--- public/src/conf/CustomerSpecificServerFunctions.js
+++ public/src/conf/CustomerSpecificServerFunctions.js
@@ -12,9 +12,9 @@
     var config, error, form, req, request,
       _this = this;
 
-    config = require('./configurationNode.js');
+    config = require('../../../conf/compiled/conf.js');
     request = require('request');
-    req = request.post(config.serverConfigurationParams.configuration.loggingService, function(error, response) {
+    req = request.post(config.all.server.service.external.logging.url, function(error, response) {
       if (!error && response.statusCode === 200) {
         return console.log("logged: " + action + " with data: " + data + " and user: " + username);
       } else {
@@ -25,6 +25,9 @@
     });
     if (username == null) {
       username = "NA";
+    }
+    if (data === "") {
+      data = "NA";
     }
     try {
       form = req.form();
@@ -102,74 +105,15 @@
     });
   };
 
-  exports.fillConfigTemplateFile = function(callback) {
-    var asyncblock, exec, fs;
-
-    fs = require('fs');
-    asyncblock = require('asyncblock');
-    exec = require('child_process').exec;
-    return asyncblock(function(flow) {
-      var config, configLines, configTemplate, enableSpecRunner, hostName, jdbcParts, line, lineParts, name, setting, settings, _i, _len;
-
-      global.deployMode = process.env.DNSDeployMode;
-      exec("java -jar ../../lib/dns-config-client.jar -m " + global.deployMode + " -c acas -d 2>/dev/null", flow.add());
-      config = flow.wait();
-      if (config.indexOf("It=works") > -1) {
-        console.log("Can't contact DNS config service. If you are doing local dev, check your VPN.");
-        process.exit(1);
-      }
-      config = config.replace(/\\/g, "");
-      configLines = config.split("\n");
-      settings = {};
-      for (_i = 0, _len = configLines.length; _i < _len; _i++) {
-        line = configLines[_i];
-        lineParts = line.split(/\=(.+)?/);
-        if (lineParts[1] !== void 0) {
-          settings[lineParts[0]] = lineParts[1];
-        }
-      }
-      configTemplate = fs.readFileSync("../public/src/conf/configurationNode_Template.js").toString();
-      for (name in settings) {
-        setting = settings[name];
-        configTemplate = configTemplate.replace(RegExp(name, "g"), setting);
-      }
-      jdbcParts = settings["acas.jdbc.url"].split(":");
-      configTemplate = configTemplate.replace(/acas.api.db.location/g, jdbcParts[0] + ":" + jdbcParts[1] + ":" + jdbcParts[2] + ":@");
-      configTemplate = configTemplate.replace(/acas.api.db.host/g, jdbcParts[3].replace("@", ""));
-      configTemplate = configTemplate.replace(/acas.api.db.port/g, jdbcParts[4]);
-      configTemplate = configTemplate.replace(/acas.api.db.name/g, jdbcParts[5]);
-      enableSpecRunner = true;
-      switch (global.deployMode) {
-        case "Dev":
-          hostName = "acas-d";
-          break;
-        case "Test":
-          hostName = "acas-t";
-          break;
-        case "Stage":
-          hostName = "acas-s";
-          break;
-        case "Prod":
-          hostName = "acas";
-          enableSpecRunner = false;
-      }
-      configTemplate = configTemplate.replace(RegExp("acas.api.hostname", "g"), hostName);
-      configTemplate = configTemplate.replace(/acas.api.enableSpecRunner/g, enableSpecRunner);
-      configTemplate = configTemplate.replace(/acas.env.logDir/g, process.env.DNSLogDirectory);
-      fs.writeFileSync("../public/src/conf/configurationNode.js", configTemplate);
-      return callback();
-    });
-  };
-
   exports.authCheck = function(user, pass, retFun) {
     var config, request,
       _this = this;
 
-    config = require('./configurationNode.js');
+    config = require('../../../conf/compiled/conf.js');
     request = require('request');
     return request({
       method: 'POST',
-      url: config.serverConfigurationParams.configuration.userAuthenticationServiceURL,
+      url: config.all.server.service.external.user.authentication.url,
       form: {
         username: user,
         password: pass
@@ -191,11 +135,11 @@
     var config, request,
       _this = this;
 
-    config = require('./configurationNode.js');
+    config = require('../../../conf/compiled/conf.js');
     request = require('request');
     return request({
       method: 'GET',
-      url: config.serverConfigurationParams.configuration.userInformationServiceURL + username,
+      url: config.all.server.service.external.user.information.url + username,
       json: true
     }, function(error, response, json) {
       if (!error && response.statusCode === 200) {
@@ -228,7 +172,7 @@
 
           if (results.indexOf("Success") >= 0) {
             try {
-              exports.logUsage("User logged in succesfully: ", "NA", username);
+              exports.logUsage("User logged in succesfully: ", "", username);
             } catch (_error) {
               error = _error;
               console.log("Exception trying to log:" + error);
@@ -236,7 +180,7 @@
             return done(null, user);
           } else {
             try {
-              exports.logUsage("User failed login: ", "NA", username);
+              exports.logUsage("User failed login: ", "", username);
             } catch (_error) {
               error = _error;
               console.log("Exception trying to log:" + error);
@@ -254,11 +198,11 @@
     var config, request,
       _this = this;
 
-    config = require('./configurationNode.js');
+    config = require('../../../conf/compiled/conf.js');
     request = require('request');
     return request({
       method: 'GET',
-      url: config.serverConfigurationParams.configuration.projectsServiceURL,
+      url: config.all.server.service.external.project.url,
       json: true
     }, function(error, response, json) {
       if (!error && response.statusCode === 200) {
