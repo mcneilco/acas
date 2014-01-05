16e8bfae3b58b92407955ff0c7c6a0d8e27d26cd
Samuel Meyer <sam@mcneilco.com>
2014-01-05 14:47:19 -0800

    preferredBatchIdService: fixing configuration for serviceType

--- routes/PreferredBatchIdService.js
+++ routes/PreferredBatchIdService.js
@@ -3,12 +3,13 @@
 
   exports.preferredBatchId = function(req, resp) {
     var config, each, request, requests, serverUtilityFunctions, serviceType, _;
+
     _ = require("underscore");
     each = require("each");
     request = require('request');
     config = require('../conf/compiled/conf.js');
     serverUtilityFunctions = require('./ServerUtilityFunctions.js');
-    serviceType = config.all.server.service.external.preferred.batchid.url;
+    serviceType = config.all.client.service.external.preferred.batchid.type;
     requests = req.body.requests;
     if (serviceType === "SeuratCmpdReg" && !global.specRunnerTestmode) {
       req.body.user = "";
@@ -19,6 +20,7 @@
       return each(requests).parallel(1).on("item", function(batchName, next) {
         var baseurl,
           _this = this;
+
         if (global.specRunnerTestmode) {
           console.log("running fake batch check");
           checkBatch_TestMode(batchName);
@@ -70,6 +72,7 @@
         });
       }).on("end", function() {
         var answer;
+
         answer = {
           error: false,
           errorMessages: [],
@@ -83,6 +86,7 @@
 
   checkBatch_TestMode = function(batchName) {
     var idComps, pref, respId;
+
     idComps = batchName.requestName.split("_");
     pref = idComps[0];
     respId = "";
