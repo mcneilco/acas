d3e5723909a16d481202f8d14104b14201640978
Brian Bolt <brian@mcneilco.com>
2014-02-26 14:59:15 -0800

    a few changes

--- bin/install.sh
+++ bin/install.sh
@@ -3,13 +3,16 @@
 #npm install -g grunt-cli
 usage ()
 {
-  echo 'Usage : Script -u <bitbucket_user> -p <bitbucket_password> -a <acas_branch>'
-  echo '                   -c <custom_repo> -b <custom_branch> -d <deploymode>'
+  echo 'Usage : Script -d <install_directory> -u <bitbucket_user> -p <bitbucket_password>'
+  echo '                -a <acas_branch> -c <custom_repo> -b <custom_branch> -m <deploy_mode>'
   exit
 }
 
 while [ "$1" != "" ]; do
 case $1 in
+        -d )           shift
+                       INSTALL_DIRECTORY=$1
+                       ;;
         -u )           shift
                        BITBUCKET_USER=$1
                        ;;
@@ -25,7 +28,7 @@ case $1 in
         -b )           shift
                        CUSTOM_BRANCH=$1
                        ;;
-        -d )           shift
+        -m )           shift
                        DEPLOYMODE=$1
                        ;;
         * )            QUERY=$1
@@ -37,13 +40,22 @@ if [ "$ACAS_BRANCH" = "" ]
 then
     usage
 fi
+if [ "$INSTALL_DIRECTORY" = "" ]
+then
+    INSTALL_DIRECTORY="."
+fi
 if [ "$BITBUCKET_USER" = "" ]
 then
-    read -p "Enter bitbucket user: " BITBUCKET_USER
+	printf "Enter bitbucket user: "
+	read BITBUCKET_USER
 fi
 if [ "$BITBUCKET_PASSWORD" = "" ]
 then
-    read -s -p "Enter bitbucket password for $BITBUCKET_USER: " BITBUCKET_PASSWORD
+	printf "Enter bitbucket password for $BITBUCKET_USER: "
+	stty -echo
+	read BITBUCKET_PASSWORD
+	stty echo
+	printf '\n'
 fi
 if [ "$CUSTOM_REPO" = "" ]
 then
@@ -54,7 +66,19 @@ then
 fi
 
 #Main
-cd $INSTALL_DIR
+
+echo "Installing acas_branch $ACAS_BRANCH to $INSTALL_DIRECTORY"
+if [ "$CUSTOM_REPO" = "" ]; then
+	echo "Not using acas_custom"
+else 
+	echo "Using custom_repo $CUSTOM_REPO on custom_branch $CUSTOM_BRANCH"
+fi
+if [ "$DEPLOYMODE" = "" ]; then
+	echo "deploy_mode not set so using standard configuration settings"
+else
+	echo "deploy_mode set to $DEPLOYMODE"
+fi
+cd $INSTALL_DIRECTORY
 if [ -h "acas" ]; then
 	rm acas
 fi
@@ -72,10 +96,13 @@ curl --digest --user $BITBUCKET_USER:$BITBUCKET_PASSWORD https://bitbucket.org/m
 ln -s acas/serverOnlyModules/blueimp-file-upload-node/ ../blueimp
 if [ "$CUSTOM_REPO" != "" ]
 then
+	echo "Installing acas_custom $CUSTOM_REPO on branch $CUSTOM_BRANCH"
     mkdir acas_custom
 	cd acas_custom
 	curl --digest --user $BITBUCKET_USER:$BITBUCKET_PASSWORD https://bitbucket.org/mcneilco/$CUSTOM_REPO/get/$CUSTOM_BRANCH.tar.gz | tar xvz --strip-components=1 
 	cd ..
+else 
+	echo "Not installing acas_custom"
 fi
 npm install
 grunt copy
@@ -86,8 +113,9 @@ Rscript install.R $ACAS_BRANCH $BITBUCKET_USER $BITBUCKET_PASSWORD
 if [ -z "config.R" ]; then
 	Rscript config.R
 fi
-export ACAS_HOME=$(pwd)/..
-export R_LIBS=$ACAS_HOME/r_libs
-R -e "library(racas);query('select * from api_protocol')"
+
+#export ACAS_HOME=$(pwd)/..
+#export R_LIBS=$ACAS_HOME/r_libs
+#R -e "library(racas);query('select * from api_protocol')"
 
 
