9142e43c2d3ec67e2337e3c478a00761ee0771d7
Brian Bolt <brian@mcneilco.com>
2014-03-05 09:12:41 -0800

    need == to have this work correctly

--- bin/acas.sh
+++ bin/acas.sh
@@ -11,7 +11,7 @@ source /dev/stdin <<< "$(cat $ACAS_HOME/conf/compiled/conf.properties | awk -f $
 
 #Export these variables so that the apache config can pick them up
 export ACAS_USER=${server_run_user}
-if [ $ACAS_USER="" ]; then
+if [ $ACAS_USER == "" ]; then
     export ACAS_USER=$(whoami)
 fi
 
