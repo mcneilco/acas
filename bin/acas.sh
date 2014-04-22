b101dee1a3c55b86663f9ca9bc744c0d764211a4
Brian Bolt <brian@mcneilco.com>
2014-04-22 14:56:26 +0000

    fixed issue with not reading init.d functions

--- bin/acas.sh
+++ bin/acas.sh
@@ -12,7 +12,12 @@
 ################################################################################
  
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
-if [ -f /etc/lsb-release ]; then
+
+if [ -f /etc/redhat-release ]; then
+    OS=`cat /etc/redhat-release | awk {'print $1}'`
+    VER=`cut -d ' ' -f 3 /etc/redhat-release`
+    . /etc/init.d/functions
+elif [ -f /etc/lsb-release ]; then
     . /etc/lsb-release
     OS=$DISTRIB_ID
     VER=$DISTRIB_RELEASE
@@ -21,16 +26,10 @@ elif [ -f /etc/debian_version ]; then
     OS=Debian  # XXX or Ubuntu??
     VER=$(cat /etc/debian_version)
     . /lib/lsb/init-functions
-elif [ -f /etc/redhat-release ]; then
-    OS=`cat /etc/redhat-release | awk {'print $1}'`
-    VER=`cut -d ' ' -f 3 /etc/redhat-release`
-    . /etc/init.d/functions
 else
     OS=$(uname -s)
     VER=$(uname -r)
 fi
-
-
 
 export PATH=/usr/local/bin:${PATH:=}
 export MANPATH=/usr/local/man:${MANPATH:=}
