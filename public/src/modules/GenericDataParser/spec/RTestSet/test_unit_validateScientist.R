9fc8bd6e2dc7f702a6ca48c1df795d203a6f8444
Jennifer Rogers <jennifer@mcneilco.com>
2014-06-09 14:06:31 -0700

    Modified validateScientist in generic_data_parser. Added a test mode, and got the function to return the empty string when the empty string was passed to it. Added tests for the validateScientist function (unit tests, not integration tests) and ran integration tests by hand.

---
+++ public/src/modules/GenericDataParser/spec/RTestSet/test_unit_validateScientist.R
@@ -0,0 +1,41 @@
+# Unit tests for data validation functions
+# Author: Jennifer Rogers, modifying tests
+#         by Sam Meyer
+# These are NOT integration tests -- we do
+# not test that the information from the internet
+# works correctly, just that the function
+# performs as expected if the response from
+# the internet is as expected
+
+library(testthat)
+source("public/src/modules/GenericDataParser/src/server/generic_data_parser.R")
+
+context("validateScientist")
+
+test_that("Scientists in the database are returned with no errors", {
+  # Note: Because this is in test mode, they don't actually have to be in the database
+  
+  errorList <<- list()
+  expect_identical("smeyer", validateScientist("smeyer", racas::applicationSettings, testMode = TRUE))
+  expect_identical(list(), errorList)
+  
+  errorList <<- list()
+  expect_identical("Brian Bolt", validateScientist("Brian Bolt", racas::applicationSettings, testMode = TRUE))
+  expect_identical(list(), errorList)
+})
+
+test_that("Scientists not in the database are returned as the empty string, and give an error", {
+  errorList <<- list()
+  expect_identical("", validateScientist("unknownUser", racas::applicationSettings, testMode = TRUE))
+  expect_identical(list("The Scientist you supplied, 'unknownUser', is not a valid name. Please enter the scientist's login name."),
+                   errorList)
+})
+
+test_that("Empty strings are returned as the empty string, with one error", {
+  errorList <<- list()
+  expect_identical("", validateScientist("", racas::applicationSettings, testMode = TRUE))
+  expect_identical(list("There was an error in validating the scientist's name: "),
+                   errorList)
+})
+
+
