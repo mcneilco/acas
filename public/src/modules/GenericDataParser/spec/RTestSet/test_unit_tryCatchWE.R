b8d3402c1c6a909f374e10042b7b47c39f27ff87
Jennifer Rogers <jennifer@mcneilco.com>
2014-06-09 15:08:23 -0700

    Unit tests for tryCatchW.E and the validate functions: validateNumeric, validateDate, and validateCharacter

---
+++ public/src/modules/GenericDataParser/spec/RTestSet/test_unit_tryCatchWE.R
@@ -0,0 +1,41 @@
+# Unit tests for "tryCatch.W.E"
+# 
+# Author: Jennifer Rogers, modifying tests
+# by Sam Meyer
+
+library(testthat)
+source("public/src/modules/GenericDataParser/src/server/generic_data_parser.R")
+
+context("tryCatch.W.E")
+
+test_that ("tryCatch.W.E (from racas > errorLogging) correctly throws no errors", {
+  throwNoErrors <- function() {return("hello world!")}
+  expect_identical(list(value="hello world!",warningList=list()), tryCatch.W.E(throwNoErrors()))
+})
+
+
+test_that("tryCatch.W.E correctly throws an error", { 
+  throwAnError <- function() {stop("Stop Now")}
+  load("public/src/modules/GenericDataParser/spec/RTestSet/IO_for_test_files/TryCatch.W.EcaughtError.Rda")
+  expect_identical(caughtError, tryCatch.W.E(throwAnError()))
+})
+
+
+test_that("tryCatch.W.E handles warnings", {
+  throwAWarning <- function() {
+    warning("This is a warning") 
+    return("hello world!")
+  }
+  load("public/src/modules/GenericDataParser/spec/RTestSet/IO_for_test_files/TryCatch.W.EcaughtWarning.Rda")
+  expect_identical(caughtWarning, tryCatch.W.E(throwAWarning()))
+})
+  
+test_that("tryCatch.W.E can throw a both warnings and errors", {
+  throwTwoWarningsAndAnError <- function() {
+    warning("This is a warning")
+    warning("This is also a warning") 
+    stop("Stop Now")
+  }
+  load("public/src/modules/GenericDataParser/spec/RTestSet/IO_for_test_files/TryCatch.W.EcaughtTwoWarningsAndAnError.Rda")
+  expect_identical(caughtWarningsAndError, tryCatch.W.E(throwTwoWarningsAndAnError()))
+})
