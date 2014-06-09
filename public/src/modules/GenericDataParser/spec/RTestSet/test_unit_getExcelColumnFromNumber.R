57b603c61110e2571353a4c19263af2596f1aaf4
Jennifer Rogers <jennifer@mcneilco.com>
2014-06-09 15:59:31 -0700

    Added tests for getExcelColumnFromNumber and getHiddenColumns.
    Note: getHiddenColumns currently contains a failing case, which is linked to bug report #193

---
+++ public/src/modules/GenericDataParser/spec/RTestSet/test_unit_getExcelColumnFromNumber.R
@@ -0,0 +1,18 @@
+# Unit Tests for getExcelColumnFromNumber
+#
+# Author: Jennifer Rogers, modifying code
+# from Sam Meyer
+
+test_that("getExcelColumnFromNumber gets the right column", {
+  expect_equal("A", getExcelColumnFromNumber(1))
+  expect_equal("AP", getExcelColumnFromNumber(42))
+  expect_equal("RFU", getExcelColumnFromNumber(12345))
+})
+
+test_that("getExcelColumnFromNumber refuses bad column numbers", {
+  load("public/src/modules/GenericDataParser/spec/RTestSet/IO_for_test_files/getExcelColumn0.Rda")
+  expect_identical(getExcelColumn0, tryCatch.W.E(getExcelColumnFromNumber(0)))
+  
+  load("public/src/modules/GenericDataParser/spec/RTestSet/IO_for_test_files/getExcelColumnNegative1.Rda")
+  expect_identical(getExcelColumnNegative1, tryCatch.W.E(getExcelColumnFromNumber(-1)))
+})
---
+++ public/src/modules/GenericDataParser/spec/RTestSet/test_unit_getHiddenColumns.R
@@ -0,0 +1,41 @@
+# Unit tests for getHiddenColumns
+# 
+# Author: Jennifer Rogers, adapted from
+# tests by Sam Meyer
+
+library(testthat)
+source("public/src/modules/GenericDataParser/src/server/generic_data_parser.R")
+
+context("getHiddenColumns")
+
+test_that("getHiddenColumns handles cases without errors", {
+  errorList <<- list()
+  expect_identical(c(FALSE,TRUE,TRUE,FALSE,FALSE), getHiddenColumns(c("Text(shown)","Text (hidden)","Date(hidden)","String","NotADatatype("), errorEnv = NULL))
+  expect_identical(list(), errorList)
+})
+
+test_that("getHiddenColumns handles cases with unrecognized entries in parentheses", {
+  errorList <<- list()
+  expect_identical(c(FALSE,FALSE), getHiddenColumns(c("Text","Text (hello world!)"), errorEnv = NULL))
+  expect_identical(list("In Datatype column B, there is an entry in the parentheses that cannot be understood: 'hello world!'. Please enter 'shown' or 'hidden'."),
+                   errorList)
+  
+})
+  
+test_that ("getHiddenColumns accepts varieties of capitalization", {
+  errorList <<- list()
+  expect_identical(c(TRUE,TRUE,FALSE,FALSE,FALSE), getHiddenColumns(c("rabbit(hidden)", "Text (Hidden)", "Date (Shown)", "Text(shown)", "NotADatatype)"), errorEnv = NULL))
+  expect_identical(list(), errorList)
+})
+
+test_that("getHiddenColumns accepts a variety of missing input", {
+  errorList <<- list()
+  expect_identical(c(FALSE, FALSE, FALSE), getHiddenColumns(c("", NA_character_, "Text ()")))
+  expect_identical(list(), errorList)  
+})
+
+test_that("getHiddenColumns doesn't error when given multiple entries in parentheses", {
+  errorList <<- list()
+  expect_identical(c(FALSE, TRUE), getHiddenColumns(c("Text (hidden) (shown)", "Text (shown) (hidden)")))
+  expect_identical(list(), errorList)
+})
