a5ba8d826d171d1113747ea7f9b72bc80628e21e
Jennifer Rogers <jennifer@mcneilco.com>
2014-06-11 15:40:29 -0700

    Rearranged tests

--- public/src/modules/GenericDataParser/spec/RTestSet/test_unit_getHiddenColumns.R
+++ public/src/modules/GenericDataParser/spec/RTestSet/test_unit_getHiddenColumns.R
@@ -19,14 +19,8 @@ test_that("getHiddenColumns handles cases with unrecognized entries in parenthes
   expect_identical(c(FALSE,FALSE), getHiddenColumns(c("Text","Text (hello world!)"), errorEnv = NULL))
   expect_identical(list("In Datatype column B, there is an entry in the parentheses that cannot be understood: 'hello world!'. Please enter 'shown' or 'hidden'."),
                    errorList)
-  
 })
   
-test_that ("getHiddenColumns accepts varieties of capitalization", {
-  errorList <<- list()
-  expect_identical(c(TRUE,TRUE,FALSE,FALSE,FALSE), getHiddenColumns(c("rabbit(hidden)", "Text (Hidden)", "Date (Shown)", "Text(shown)", "NotADatatype)"), errorEnv = NULL))
-  expect_identical(list(), errorList)
-})
 
 test_that("getHiddenColumns accepts a variety of missing input", {
   errorList <<- list()
@@ -37,5 +31,11 @@ test_that("getHiddenColumns accepts a variety of missing input", {
 test_that("getHiddenColumns doesn't error when given multiple entries in parentheses", {
   errorList <<- list()
   expect_identical(c(FALSE, TRUE), getHiddenColumns(c("Text (hidden) (shown)", "Text (shown) (hidden)")))
+  expect_identical(list(), errorList)
+})
+
+test_that ("getHiddenColumns accepts varieties of capitalization", {
+  errorList <<- list()
+  expect_identical(c(TRUE,TRUE,FALSE,FALSE,FALSE), getHiddenColumns(c("rabbit(hidden)", "Text (Hidden)", "Date (Shown)", "Text(shown)", "NotADatatype)"), errorEnv = NULL))
   expect_identical(list(), errorList)
 })
