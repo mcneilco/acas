c79c0a8e6147793cd7bb1376b603632ad48a8a63
Brian Bolt <brian@mcneilco.com>
2014-08-08 15:27:38 -0700

    keep original order

--- public/src/modules/PrimaryScreen/src/server/PrimaryAnalysisAnalyzeData.R
+++ public/src/modules/PrimaryScreen/src/server/PrimaryAnalysisAnalyzeData.R
@@ -32,14 +32,18 @@ dataframe_to_csvstring <- function(x) {
 normalizeData <- function() {
     csv_data <- rawToChar(receiveBin(-1))
     data <- fread(csv_data)
+    data[, originalOrder:=1:nrow(data)]
     setkey(data, assayBarcode, wellReference)
     readColumnIndexes <- which(grepl("{R[0-9].*}",names(data), perl = TRUE))
     readColumnNames <- names(data)[readColumnIndexes]
     normalizedNames <- paste0(readColumnNames," [NORMALIZED]")
+    keyColumns <- c("assayBarcode","wellReference")
     normalizedData <- data[ , get("normalizedNames"):={
              lapply(readColumnNames, function(x) get(x)*3)
-             }, by = c("assayBarcode","wellReference")]
-
+             }, by = keyColumns]
+    setkey(data,originalOrder)
+    keepColumns <- c(keyColumns,normalizedNames)
+    data[ , setdiff(colnames(data),keepColumns):=NULL, with = FALSE]
     csv_data <- dataframe_to_csvstring(normalizedData)
     setHeader("Access-Control-Allow-Origin","*")
     setHeader("Content-Length",nchar(csv_data))
