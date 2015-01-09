8fdb83388cbc24aba26728c6d56bb4a2f743667e
evagao <eva@mcneilco.com>
2015-01-08 16:52:27 -0800

    lsThing-eva: added untracked AttachFileTestJSON files

---
+++ public/javascripts/spec/testFixtures/AttachFileTestJSON.js
@@ -0,0 +1,14 @@
+(function() {
+  (function(exports) {
+    return exports.attachFileInfo = [
+      {
+        fileType: "hplc",
+        fileValue: "hplc.xlsx"
+      }, {
+        fileType: "nmr",
+        fileVaue: "nmr.xls"
+      }
+    ];
+  })((typeof process === "undefined" || !process.versions ? window.attachFileTestJSON = window.attachFileTestJSON || {} : exports));
+
+}).call(this);
