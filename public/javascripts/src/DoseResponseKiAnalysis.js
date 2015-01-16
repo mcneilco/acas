582da99da2fa2ca9517d5118b7300fa568897af2
Brian Bolt <brian@mcneilco.com>
2015-01-16 08:54:24 -0800

    specs broken

---
+++ public/javascripts/src/DoseResponseKiAnalysis.js
@@ -0,0 +1,246 @@
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  window.DoseResponseKiAnalysisParameters = (function(_super) {
+    __extends(DoseResponseKiAnalysisParameters, _super);
+
+    function DoseResponseKiAnalysisParameters() {
+      this.fixCompositeClasses = __bind(this.fixCompositeClasses, this);
+      return DoseResponseKiAnalysisParameters.__super__.constructor.apply(this, arguments);
+    }
+
+    DoseResponseKiAnalysisParameters.prototype.defaults = {
+      inactiveThreshold: 20,
+      inverseAgonistMode: true,
+      max: new Backbone.Model({
+        limitType: 'none'
+      }),
+      min: new Backbone.Model({
+        limitType: 'none'
+      }),
+      kd: new Backbone.Model,
+      ligandConc: new Backbone.Model
+    };
+
+    DoseResponseKiAnalysisParameters.prototype.initialize = function() {
+      return this.fixCompositeClasses();
+    };
+
+    DoseResponseKiAnalysisParameters.prototype.fixCompositeClasses = function() {
+      if (!(this.get('max') instanceof Backbone.Model)) {
+        this.set({
+          max: new Backbone.Model(this.get('max'))
+        });
+      }
+      if (!(this.get('min') instanceof Backbone.Model)) {
+        this.set({
+          min: new Backbone.Model(this.get('min'))
+        });
+      }
+      if (!(this.get('kd') instanceof Backbone.Model)) {
+        this.set({
+          kd: new Backbone.Model(this.get('kd'))
+        });
+      }
+      if (!(this.get('ligandConc') instanceof Backbone.Model)) {
+        return this.set({
+          ligandConc: new Backbone.Model(this.get('ligandConc'))
+        });
+      }
+    };
+
+    DoseResponseKiAnalysisParameters.prototype.validate = function(attrs) {
+      var errors, limitType;
+      errors = [];
+      limitType = attrs.min.get('limitType');
+      if ((limitType === "pin" || limitType === "limit") && _.isNaN(attrs.min.get('value'))) {
+        errors.push({
+          attribute: 'min_value',
+          message: "Min threshold value must be set when limit type is pin or limit"
+        });
+      }
+      limitType = attrs.max.get('limitType');
+      if ((limitType === "pin" || limitType === "limit") && _.isNaN(attrs.max.get('value'))) {
+        errors.push({
+          attribute: 'max_value',
+          message: "Max threshold value must be set when limit type is pin or limit"
+        });
+      }
+      if (_.isNaN(attrs.kd.get('value'))) {
+        errors.push({
+          attribute: 'kd_value',
+          message: "Kd threshold value must be set"
+        });
+      }
+      if (_.isNaN(attrs.ligandConc.get('value'))) {
+        errors.push({
+          attribute: 'ligandConc_value',
+          message: "Ligand Conc. threshold value must be set"
+        });
+      }
+      if (_.isNaN(attrs.inactiveThreshold)) {
+        errors.push({
+          attribute: 'inactiveThreshold',
+          message: "Inactive threshold value must be set to a number"
+        });
+      }
+      if (errors.length > 0) {
+        return errors;
+      } else {
+        return null;
+      }
+    };
+
+    return DoseResponseKiAnalysisParameters;
+
+  })(Backbone.Model);
+
+  window.DoseResponseKiAnalysisParametersController = (function(_super) {
+    __extends(DoseResponseKiAnalysisParametersController, _super);
+
+    function DoseResponseKiAnalysisParametersController() {
+      this.handleMinLimitTypeChanged = __bind(this.handleMinLimitTypeChanged, this);
+      this.handleMaxLimitTypeChanged = __bind(this.handleMaxLimitTypeChanged, this);
+      this.handleInverseAgonistModeChanged = __bind(this.handleInverseAgonistModeChanged, this);
+      this.handleInactiveThresholdMoved = __bind(this.handleInactiveThresholdMoved, this);
+      this.handleInactiveThresholdChanged = __bind(this.handleInactiveThresholdChanged, this);
+      this.updateModel = __bind(this.updateModel, this);
+      this.render = __bind(this.render, this);
+      return DoseResponseKiAnalysisParametersController.__super__.constructor.apply(this, arguments);
+    }
+
+    DoseResponseKiAnalysisParametersController.prototype.template = _.template($("#DoseResponseKiAnalysisParametersView").html());
+
+    DoseResponseKiAnalysisParametersController.prototype.autofillTemplate = _.template($("#DoseResponseKiAnalysisParametersAutofillView").html());
+
+    DoseResponseKiAnalysisParametersController.prototype.events = {
+      "change .bv_inverseAgonistMode": "handleInverseAgonistModeChanged",
+      "click .bv_max_limitType_none": "handleMaxLimitTypeChanged",
+      "click .bv_max_limitType_pin": "handleMaxLimitTypeChanged",
+      "click .bv_max_limitType_limit": "handleMaxLimitTypeChanged",
+      "click .bv_min_limitType_none": "handleMinLimitTypeChanged",
+      "click .bv_min_limitType_pin": "handleMinLimitTypeChanged",
+      "click .bv_min_limitType_limit": "handleMinLimitTypeChanged",
+      "change .bv_max_value": "attributeChanged",
+      "change .bv_min_value": "attributeChanged",
+      "change .bv_kd_value": "attributeChanged",
+      "change .bv_ligandConc_value": "attributeChanged"
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.initialize = function() {
+      $(this.el).html(this.template());
+      this.errorOwnerName = 'DoseResponseKiAnalysisParametersController';
+      return this.setBindings();
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.render = function() {
+      this.$('.bv_autofillSection').empty();
+      this.$('.bv_autofillSection').html(this.autofillTemplate($.parseJSON(JSON.stringify(this.model))));
+      this.$('.bv_inactiveThreshold').slider({
+        value: this.model.get('inactiveThreshold'),
+        min: 0,
+        max: 100
+      });
+      this.$('.bv_inactiveThreshold').on('slide', this.handleInactiveThresholdMoved);
+      this.$('.bv_inactiveThreshold').on('slidestop', this.handleInactiveThresholdChanged);
+      this.updateThresholdDisplay(this.model.get('inactiveThreshold'));
+      this.setFormTitle();
+      return this;
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.updateThresholdDisplay = function(val) {
+      return this.$('.bv_inactiveThresholdDisplay').html(val);
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.setThresholdEnabledState = function() {
+      if (this.model.get('inverseAgonistMode')) {
+        return this.$('.bv_inactiveThreshold').slider('disable');
+      } else {
+        return this.$('.bv_inactiveThreshold').slider('enable');
+      }
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.updateModel = function() {
+      this.model.get('max').set({
+        value: parseFloat(UtilityFunctions.prototype.getTrimmedInput(this.$('.bv_max_value')))
+      });
+      this.model.get('min').set({
+        value: parseFloat(UtilityFunctions.prototype.getTrimmedInput(this.$('.bv_min_value')))
+      });
+      this.model.get('kd').set({
+        value: parseFloat(UtilityFunctions.prototype.getTrimmedInput(this.$('.bv_kd_value')))
+      });
+      this.model.get('ligandConc').set({
+        value: parseFloat(UtilityFunctions.prototype.getTrimmedInput(this.$('.bv_ligandConc_value')))
+      });
+      this.model.set({
+        inverseAgonistMode: this.$('.bv_inverseAgonistMode').is(":checked")
+      }, {
+        silent: true
+      });
+      return this.model.trigger('change');
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.handleInactiveThresholdChanged = function(event, ui) {
+      this.model.set({
+        'inactiveThreshold': ui.value
+      });
+      this.updateThresholdDisplay(this.model.get('inactiveThreshold'));
+      return this.attributeChanged;
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.handleInactiveThresholdMoved = function(event, ui) {
+      return this.updateThresholdDisplay(ui.value);
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.handleInverseAgonistModeChanged = function() {
+      return this.attributeChanged();
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.handleMaxLimitTypeChanged = function() {
+      var radioValue;
+      radioValue = this.$("input[name='bv_max_limitType']:checked").val();
+      this.model.get('max').set({
+        limitType: radioValue,
+        silent: true
+      });
+      if (radioValue === 'none') {
+        this.$('.bv_max_value').attr('disabled', 'disabled');
+      } else {
+        this.$('.bv_max_value').removeAttr('disabled');
+      }
+      return this.attributeChanged();
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.handleMinLimitTypeChanged = function() {
+      var radioValue;
+      radioValue = this.$("input[name='bv_min_limitType']:checked").val();
+      this.model.get('min').set({
+        limitType: radioValue
+      });
+      if (radioValue === 'none') {
+        this.$('.bv_min_value').attr('disabled', 'disabled');
+      } else {
+        this.$('.bv_min_value').removeAttr('disabled');
+      }
+      return this.attributeChanged();
+    };
+
+    DoseResponseKiAnalysisParametersController.prototype.setFormTitle = function(title) {
+      if (title != null) {
+        this.formTitle = title;
+        return this.$(".bv_formTitle").html(this.formTitle);
+      } else if (this.formTitle != null) {
+        return this.$(".bv_formTitle").html(this.formTitle);
+      } else {
+        return this.formTitle = this.$(".bv_formTitle").html();
+      }
+    };
+
+    return DoseResponseKiAnalysisParametersController;
+
+  })(AbstractFormController);
+
+}).call(this);
---
+++ public/src/modules/PrimaryScreen/src/client/DoseResponseKiAnalysis.coffee
@@ -0,0 +1,149 @@
+class window.DoseResponseKiAnalysisParameters extends Backbone.Model
+	defaults:
+		inactiveThreshold: 20
+		inverseAgonistMode: true
+		max: new Backbone.Model limitType: 'none'
+		min: new Backbone.Model limitType: 'none'
+		kd: new Backbone.Model
+		ligandConc: new Backbone.Model
+
+	initialize: ->
+		@fixCompositeClasses()
+
+	fixCompositeClasses: =>
+		if @get('max') not instanceof Backbone.Model
+			@set max: new Backbone.Model(@get('max'))
+		if @get('min') not instanceof Backbone.Model
+			@set min: new Backbone.Model(@get('min'))
+		if @get('kd') not instanceof Backbone.Model
+			@set kd: new Backbone.Model(@get('kd'))
+		if @get('ligandConc') not instanceof Backbone.Model
+			@set ligandConc: new Backbone.Model(@get('ligandConc'))
+
+
+	validate: (attrs) ->
+		errors = []
+
+		limitType = attrs.min.get('limitType')
+		if (limitType == "pin" || limitType == "limit") && _.isNaN(attrs.min.get('value'))
+			errors.push
+				attribute: 'min_value'
+				message: "Min threshold value must be set when limit type is pin or limit"
+		limitType = attrs.max.get('limitType')
+		if (limitType == "pin" || limitType == "limit") && _.isNaN(attrs.max.get('value'))
+			errors.push
+				attribute: 'max_value'
+				message: "Max threshold value must be set when limit type is pin or limit"
+		if (_.isNaN(attrs.kd.get('value')))
+			errors.push
+				attribute: 'kd_value'
+				message: "Kd threshold value must be set"
+		if (_.isNaN(attrs.ligandConc.get('value')))
+			errors.push
+				attribute: 'ligandConc_value'
+				message: "Ligand Conc. threshold value must be set"
+		if  _.isNaN(attrs.inactiveThreshold)
+			errors.push
+				attribute: 'inactiveThreshold'
+				message: "Inactive threshold value must be set to a number"
+		if errors.length > 0
+			return errors
+		else
+			return null
+
+class window.DoseResponseKiAnalysisParametersController extends AbstractFormController
+	template: _.template($("#DoseResponseKiAnalysisParametersView").html())
+	autofillTemplate: _.template($("#DoseResponseKiAnalysisParametersAutofillView").html())
+
+	events:
+		"change .bv_inverseAgonistMode": "handleInverseAgonistModeChanged"
+		"click .bv_max_limitType_none": "handleMaxLimitTypeChanged"
+		"click .bv_max_limitType_pin": "handleMaxLimitTypeChanged"
+		"click .bv_max_limitType_limit": "handleMaxLimitTypeChanged"
+		"click .bv_min_limitType_none": "handleMinLimitTypeChanged"
+		"click .bv_min_limitType_pin": "handleMinLimitTypeChanged"
+		"click .bv_min_limitType_limit": "handleMinLimitTypeChanged"
+		"change .bv_max_value": "attributeChanged"
+		"change .bv_min_value": "attributeChanged"
+		"change .bv_kd_value": "attributeChanged"
+		"change .bv_ligandConc_value": "attributeChanged"
+
+	initialize: ->
+		$(@el).html @template()
+		@errorOwnerName = 'DoseResponseKiAnalysisParametersController'
+		@setBindings()
+
+	render: =>
+		@$('.bv_autofillSection').empty()
+		@$('.bv_autofillSection').html @autofillTemplate($.parseJSON(JSON.stringify(@model)))
+		@$('.bv_inactiveThreshold').slider
+			value: @model.get('inactiveThreshold')
+			min: 0
+			max: 100
+		@$('.bv_inactiveThreshold').on 'slide', @handleInactiveThresholdMoved
+		@$('.bv_inactiveThreshold').on 'slidestop', @handleInactiveThresholdChanged
+		@updateThresholdDisplay(@model.get 'inactiveThreshold')
+		@setFormTitle()
+#		@setThresholdEnabledState()
+		@
+
+	updateThresholdDisplay: (val)->
+		@$('.bv_inactiveThresholdDisplay').html val
+
+	setThresholdEnabledState: ->
+		if @model.get 'inverseAgonistMode'
+			@$('.bv_inactiveThreshold').slider('disable')
+		else
+			@$('.bv_inactiveThreshold').slider('enable')
+
+	updateModel: =>
+		@model.get('max').set
+			value: parseFloat(UtilityFunctions::getTrimmedInput @$('.bv_max_value'))
+		@model.get('min').set
+			value: parseFloat(UtilityFunctions::getTrimmedInput @$('.bv_min_value'))
+		@model.get('kd').set
+			value: parseFloat(UtilityFunctions::getTrimmedInput @$('.bv_kd_value'))
+		@model.get('ligandConc').set
+			value: parseFloat(UtilityFunctions::getTrimmedInput @$('.bv_ligandConc_value'))
+		@model.set inverseAgonistMode: @$('.bv_inverseAgonistMode').is(":checked"),
+			silent: true
+		@model.trigger 'change'
+
+	handleInactiveThresholdChanged: (event, ui) =>
+		@model.set 'inactiveThreshold': ui.value
+		@updateThresholdDisplay(@model.get 'inactiveThreshold')
+		@attributeChanged
+
+	handleInactiveThresholdMoved: (event, ui) =>
+		@updateThresholdDisplay(ui.value)
+
+	handleInverseAgonistModeChanged: =>
+		@attributeChanged()
+
+	handleMaxLimitTypeChanged: =>
+		radioValue = @$("input[name='bv_max_limitType']:checked").val()
+
+		@model.get('max').set limitType: radioValue, silent: true
+		if radioValue == 'none'
+			@$('.bv_max_value').attr('disabled','disabled')
+		else
+			@$('.bv_max_value').removeAttr('disabled')
+		@attributeChanged()
+
+	handleMinLimitTypeChanged: =>
+		radioValue = @$("input[name='bv_min_limitType']:checked").val()
+		@model.get('min').set limitType: radioValue
+		if radioValue == 'none'
+			@$('.bv_min_value').attr('disabled','disabled')
+		else
+			@$('.bv_min_value').removeAttr('disabled')
+		@attributeChanged()
+
+	setFormTitle: (title) ->
+		if title?
+			@formTitle = title
+			@$(".bv_formTitle").html @formTitle
+		else if @formTitle?
+			@$(".bv_formTitle").html @formTitle
+		else
+			@formTitle = @$(".bv_formTitle").html()
