28ff796a87d668dbc22d61f999a4098009fe054a
evagao <eva@mcneilco.com>
2015-03-04 10:14:46 -0800

    development-added async node module and fixed bugs for DNETRPLC-281

--- conf/PrepareModuleConfJSON.coffee
+++ conf/PrepareModuleConfJSON.coffee
@@ -5,101 +5,80 @@ allModuleConfJSFiles = glob.sync "../public/javascripts/conf/*.js"
 for fileName in allModuleConfJSFiles
 	data = require fileName
 	jsonfilestring = JSON.stringify data
-	newFileName = fileName.replace "conf", "conf/confJSON"
+	newFileName = fileName.replace "conf", "conf/confJSON/moduleJSON"
 	newFileName = newFileName.replace ".js", ".json"
 	fs.writeFileSync newFileName, jsonfilestring
 
 typeKinds = [
 	"codetables"
-	"containerkinds"
 	"containertypes"
-	"ddictkinds"
+	"containerkinds"
 	"ddicttypes"
-	"experimentkinds"
+	"ddictkinds"
 	"experimenttypes"
-	"interactionkinds"
+	"experimentkinds"
 	"interactiontypes"
+	"interactionkinds"
+	"labeltypes"
 	"labelkinds"
 	"labelsequences"
-	"labeltypes"
-	"operatorkinds"
 	"operatortypes"
-	"protocolkinds"
+	"operatorkinds"
 	"protocoltypes"
-	"statekinds"
+	"protocolkinds"
 	"statetypes"
-	"thingkinds"
+	"statekinds"
 	"thingtypes"
-	"unitkinds"
+	"thingkinds"
 	"unittypes"
-	"valuekinds"
+	"unitkinds"
 	"valuetypes"
+	"valuekinds"
 ]
-allModuleConfJSONFiles = glob.sync "../public/javascripts/conf/confJSON/*.json"
+allModuleConfJSONFiles = glob.sync "../public/javascripts/conf/confJSON/moduleJSON/*.json"
+allModulesTypesAndKinds = {}
+
 for fileName in allModuleConfJSONFiles
-	data = require fileName
+	moduleData = require fileName
 	for typeOrKind in typeKinds
-		if data.typeKindList[typeOrKind]?
-			for value in data.typeKindList[typeOrKind]
-				config = require '../conf/compiled/conf.js'
-				baseurl = config.all.client.service.persistence.fullpath+"setup/"+typeOrKind
-				request = require 'request'
-				if value.kindName? and value.typeName?
-					console.log "trying to save typeName: " + value.typeName + " and kindName: " + value.kindName
-				else if value.typeName?
-					console.log "trying to save typeName: " + value.typeName
+		if moduleData.typeKindList[typeOrKind]?
+			value = moduleData.typeKindList[typeOrKind]
+			if allModulesTypesAndKinds[typeOrKind]?
+				compiledTypesAndKinds = allModulesTypesAndKinds[typeOrKind]
+				compiledTypesAndKinds.push value...
+			else
+				allModulesTypesAndKinds[typeOrKind] = value
+jsonfilestring = JSON.stringify allModulesTypesAndKinds
+compiledModuleConfsFileName = "../public/javascripts/conf/confJSON/CompiledModuleConfJSONs.json"
+fs.writeFileSync compiledModuleConfsFileName, jsonfilestring
+
+async = require 'async'
+request = require 'request'
+data = require '../public/javascripts/conf/confJSON/CompiledModuleConfJSONs.json'
+config = require '../conf/compiled/conf.js'
+
+async.forEachSeries typeKinds, ((typeOrKind, callback) ->
+	baseurl = config.all.client.service.persistence.fullpath+"setup/"+typeOrKind
+	if data[typeOrKind]?
+		console.log "trying to save " + typeOrKind
+		request(
+			method: 'POST'
+			url: baseurl
+			body: JSON.stringify data[typeOrKind]
+			json: true
+			headers:
+				"Content-Type": 'application/json'
+			, (error, response, json) =>
+				if !error && response.statusCode == 201
+					console.log "successfully added " + typeOrKind
 				else
-					console.log "trying to save " + typeOrKind
-				request(
-					method: 'POST'
-					url: baseurl
-					body: JSON.stringify [value]
-					json: true
-					headers:
-						"Content-Type": 'application/json'
-				, (error, response, json) =>
-					unless !error && response.statusCode == 201
-						console.log 'got ajax error trying to setup type/kind'
-						console.log error
-						console.log json
-
-				)
-
-#				$.ajax
-#					type: 'POST'
-#					url: "/api/setup/"+typeOrKind
-#					data:
-#						JSON.stringify value
-#	#					JSON.stringify(codeEntry:(selectedModel))
-#					contentType: 'application/json'
-#					dataType: 'json'
-#					success: (response) =>
-#						console.log "successful post"
-#					error: (err) =>
-#						alert 'could not add option to code table'
-#						@serviceReturn = null
-
-#allCodeTables = []
-#allCodeTableTypesAndKinds = []
-#currentTypeAndKind = {}
-#
-#for fileName in allCodeTableFiles
-#	codeTableFile = require fileName
-#	for codeTable in codeTableFile['codetableValues']
-#		type = codeTable['type']
-#		kind = codeTable['kind']
-#		currentTypeAndKind['type'] = type
-#		currentTypeAndKind['kind'] = kind
-#		if _.findWhere(allCodeTableTypesAndKinds, currentTypeAndKind) is undefined
-#			allCodeTableTypesAndKinds.push [{type:codeTable['type'], kind:codeTable['kind']}]...
-#			allCodeTables.push codeTable
-#		else
-#			console.log "Error: code table for type: " + type + "and kind: " + kind + " already stored"
-#			process.exit -1
-#
-#jsonallcodetablesstring = JSON.stringify allCodeTables
-#allCodeTablesFileName = "../public/javascripts/spec/testFixtures/CodeTableJSON.js"
-#jsonallcodetablesstring = "exports.codes = " + jsonallcodetablesstring
-#fs.writeFileSync allCodeTablesFileName, jsonallcodetablesstring
-
-
+					console.log 'got ajax error trying to setup type/kind ' + typeOrKind
+					console.log error
+					console.log json
+				callback()
+		)
+	else
+		console.log "no "+typeOrKind+" to save"
+		callback()
+), (err) ->
+	console.log "done adding types and kinds"
--- conf/PrepareModuleConfJSON.js
+++ conf/PrepareModuleConfJSON.js
@@ -1,5 +1,5 @@
 (function() {
-  var allModuleConfJSFiles, allModuleConfJSONFiles, baseurl, config, data, fileName, fs, glob, jsonfilestring, newFileName, request, typeKinds, typeOrKind, value, _i, _j, _k, _l, _len, _len1, _len2, _len3, _ref;
+  var allModuleConfJSFiles, allModuleConfJSONFiles, allModulesTypesAndKinds, async, compiledModuleConfsFileName, compiledTypesAndKinds, config, data, fileName, fs, glob, jsonfilestring, moduleData, newFileName, request, typeKinds, typeOrKind, value, _i, _j, _k, _len, _len1, _len2;
 
   fs = require('fs');
 
@@ -11,54 +11,79 @@
     fileName = allModuleConfJSFiles[_i];
     data = require(fileName);
     jsonfilestring = JSON.stringify(data);
-    newFileName = fileName.replace("conf", "conf/confJSON");
+    newFileName = fileName.replace("conf", "conf/confJSON/moduleJSON");
     newFileName = newFileName.replace(".js", ".json");
     fs.writeFileSync(newFileName, jsonfilestring);
   }
 
-  typeKinds = ["codetables", "containerkinds", "containertypes", "ddictkinds", "ddicttypes", "experimentkinds", "experimenttypes", "interactionkinds", "interactiontypes", "labelkinds", "labelsequences", "labeltypes", "operatorkinds", "operatortypes", "protocolkinds", "protocoltypes", "statekinds", "statetypes", "thingkinds", "thingtypes", "unitkinds", "unittypes", "valuekinds", "valuetypes"];
+  typeKinds = ["codetables", "containertypes", "containerkinds", "ddicttypes", "ddictkinds", "experimenttypes", "experimentkinds", "interactiontypes", "interactionkinds", "labeltypes", "labelkinds", "labelsequences", "operatortypes", "operatorkinds", "protocoltypes", "protocolkinds", "statetypes", "statekinds", "thingtypes", "thingkinds", "unittypes", "unitkinds", "valuetypes", "valuekinds"];
 
-  allModuleConfJSONFiles = glob.sync("../public/javascripts/conf/confJSON/*.json");
+  allModuleConfJSONFiles = glob.sync("../public/javascripts/conf/confJSON/moduleJSON/*.json");
+
+  allModulesTypesAndKinds = {};
 
   for (_j = 0, _len1 = allModuleConfJSONFiles.length; _j < _len1; _j++) {
     fileName = allModuleConfJSONFiles[_j];
-    data = require(fileName);
+    moduleData = require(fileName);
     for (_k = 0, _len2 = typeKinds.length; _k < _len2; _k++) {
       typeOrKind = typeKinds[_k];
-      if (data.typeKindList[typeOrKind] != null) {
-        _ref = data.typeKindList[typeOrKind];
-        for (_l = 0, _len3 = _ref.length; _l < _len3; _l++) {
-          value = _ref[_l];
-          config = require('../conf/compiled/conf.js');
-          baseurl = config.all.client.service.persistence.fullpath + "setup/" + typeOrKind;
-          request = require('request');
-          if ((value.kindName != null) && (value.typeName != null)) {
-            console.log("trying to save typeName: " + value.typeName + " and kindName: " + value.kindName);
-          } else if (value.typeName != null) {
-            console.log("trying to save typeName: " + value.typeName);
-          } else {
-            console.log("trying to save " + typeOrKind);
-          }
-          request({
-            method: 'POST',
-            url: baseurl,
-            body: JSON.stringify([value]),
-            json: true,
-            headers: {
-              "Content-Type": 'application/json'
-            }
-          }, (function(_this) {
-            return function(error, response, json) {
-              if (!(!error && response.statusCode === 201)) {
-                console.log('got ajax error trying to setup type/kind');
-                console.log(error);
-                return console.log(json);
-              }
-            };
-          })(this));
+      if (moduleData.typeKindList[typeOrKind] != null) {
+        value = moduleData.typeKindList[typeOrKind];
+        if (allModulesTypesAndKinds[typeOrKind] != null) {
+          compiledTypesAndKinds = allModulesTypesAndKinds[typeOrKind];
+          compiledTypesAndKinds.push.apply(compiledTypesAndKinds, value);
+        } else {
+          allModulesTypesAndKinds[typeOrKind] = value;
         }
       }
     }
   }
+
+  jsonfilestring = JSON.stringify(allModulesTypesAndKinds);
+
+  compiledModuleConfsFileName = "../public/javascripts/conf/confJSON/CompiledModuleConfJSONs.json";
+
+  fs.writeFileSync(compiledModuleConfsFileName, jsonfilestring);
+
+  async = require('async');
+
+  request = require('request');
+
+  data = require('../public/javascripts/conf/confJSON/CompiledModuleConfJSONs.json');
+
+  config = require('../conf/compiled/conf.js');
+
+  async.forEachSeries(typeKinds, (function(typeOrKind, callback) {
+    var baseurl;
+    baseurl = config.all.client.service.persistence.fullpath + "setup/" + typeOrKind;
+    if (data[typeOrKind] != null) {
+      console.log("trying to save " + typeOrKind);
+      return request({
+        method: 'POST',
+        url: baseurl,
+        body: JSON.stringify(data[typeOrKind]),
+        json: true,
+        headers: {
+          "Content-Type": 'application/json'
+        }
+      }, (function(_this) {
+        return function(error, response, json) {
+          if (!error && response.statusCode === 201) {
+            console.log("successfully added " + typeOrKind);
+          } else {
+            console.log('got ajax error trying to setup type/kind ' + typeOrKind);
+            console.log(error);
+            console.log(json);
+          }
+          return callback();
+        };
+      })(this));
+    } else {
+      console.log("no " + typeOrKind + " to save");
+      return callback();
+    }
+  }), function(err) {
+    return console.log("done adding types and kinds");
+  });
 
 }).call(this);
