f1a2c0f64af02bfe1c9a8dba6443b90010befb66
Brian Bolt <brian@mcneilco.com>
2015-03-18 15:04:05 -0700

    removing save session

--- public/src/modules/CurveAnalysis/src/server/renderCurveCachedData.R
+++ public/src/modules/CurveAnalysis/src/server/renderCurveCachedData.R
@@ -28,7 +28,6 @@ renderCurve <- function(getParams) {
   parsedParams <- racas::parse_params_curve_render_dr(getParams)
 
   # GET Cached Curve Data
-  saveSession("/tmp/bl")
   data <- racas::get_cached_fit_data_curve_id(parsedParams$curveIds, globalConnect = TRUE)
   data$parameters <- data$parameters[!is.null(category) && category %in% c("inactive","potent"), c("fittedmax", "fittedmin") := {
                       pts <- data$points[curveId == curveId]
