10c3e6ab16d24a2de77049c63c80dcc317447458
kdwelle <kaitlyn@mcneilco.com>
2015-06-12 17:29:50 -0700

    added sorting for acas objects (i.e. sorting on < and > operators)

---
+++ public/src/lib/dataTables/js/acasSorting.js
@@ -0,0 +1,56 @@
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+
+// Sorting for common data returned by ACAS
+// All values prefaced with a "<" sign are sorted as smallest
+// All values prefaced with a ">" sign are sorted as largest
+// Also handles scientific notation
+
+
+    "acas-pre": function ( a ) {
+        var operator =a[0];
+        if (operator == ">" || operator == "<") {
+            return operator + parseFloat(a.slice(1) );
+        }
+        return (parseFloat( a ) || a);
+    },
+
+    "acas-asc": function ( a, b ) {
+        if (a[0] == "<"){
+            if (b[0] == "<"){
+                return ((a.split(1) < b.split(1)) ? -1 : ((a.split(1) > b.split(1)) ? 1 : 0));
+            }else{return -1;}
+        }if (a[0] == ">"){
+            if (b[0] == ">"){
+                return ((a.split(1) < b.split(1)) ? -1 : ((a.split(1) > b.split(1)) ? 1 : 0));
+            }else{return 1;}
+        }else {
+            if (b[0] == "<") {
+                return 1;
+            }
+            if (b[0] == ">") {
+                return -1;
+            }
+        }
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    },
+
+    "acas-desc": function ( a, b ) {
+        if (a[0] == "<"){
+            if (b[0] == "<"){
+                return ((a.split(1) < b.split(1)) ? 1 : ((a.split(1) > b.split(1)) ? -1 : 0));
+            }else{return 1;}
+        }if (a[0] == ">"){
+            if (b[0] == ">"){
+                return ((a.split(1) < b.split(1)) ? 1 : ((a.split(1) > b.split(1)) ? -1 : 0));
+            }else{return -1;}
+        }else {
+            if (b[0] == "<") {
+                return -1;
+            }
+            if (b[0] == ">") {
+                return 1;
+            }
+        }
+        return ((a < b) ? 1 : ((a > b) ? -1 : 0));
+    }
+} );
