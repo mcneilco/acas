95a8d513fbd5bbbdac90aa8bbf5db2baca4996f6
Brian Bolt <brian@mcneilco.com>
2016-02-01 13:59:31 -0800

    using env_file in compose instead of mounting files

--- bin/acas-docker.sh
+++ bin/acas-docker.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 
-# Check if mounted environments exists, if not then use default environments to export variables before prepare config files runs
-if [ -d "/mnt/environments" ];then
-    envPath="/mnt/environments"
-else
-    export APP_NAME=${APP_NAME:=acas}
-    envPath="bin/environments"
-fi
-for f in $envPath/*/*.env; do echo Using $f;source $f;export $(cut -d= -f1 $f); done
-
 # Run Prepare config files as the compiled directory should be empty
 if [ $PREPARE_CONFIG_FILES == "true" ]; then
-    cd src/javascripts/BuildUtilities
-    node PrepareConfigFiles.js
-    cd ../../..
+    grunt execute:prepare_config_files
 fi
 
 #Once tomcat is availble then try and run prepare module conf json if in demo mode
