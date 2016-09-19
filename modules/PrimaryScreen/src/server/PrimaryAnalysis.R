f6ff4870ab5a380a8d3b6c269b5a6c5113715ec3
Brian Bolt <brian@mcneilco.com>
2016-09-19 15:13:29 +0000

    fixing issue with setkey on wrong object

--- modules/PrimaryScreen/src/server/PrimaryAnalysis.R
+++ modules/PrimaryScreen/src/server/PrimaryAnalysis.R
@@ -2070,7 +2070,7 @@ runMain <- function(folderToParse, user, dryRun, testMode, experimentId, inputPa
 
     resultTable[is.na(cmpdConc)]$wellType <- "BLANK"
     checkControls(resultTable, normalizationDataFrame)
-    setkeyv(instrumentData$assayData,c('assayBarcode', 'row', 'well'))
+    setkeyv(resultTable,c('assayBarcode', 'row', 'well'))
     setkeyv(instrumentData$assayData,c('assayBarcode', 'rowName', 'wellReference'))
     resultTable[, well:= instrumentData$assayData$wellReference]
     save(resultTable, file=file.path(parsedInputFileLocation, "primaryAnalysis-resultTable.Rda"))
