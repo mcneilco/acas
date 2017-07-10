2a1eb56a62d9477f1166d1c34ec011b8c6630211
Matthew Shaw <matt@mcneilco.com>
2017-07-10 12:46:53 -0600

    Added dependency: BlockUI.js

--- modules/BuildUtilities/src/server/routes/RequiredClientScripts_template.js
+++ modules/BuildUtilities/src/server/routes/RequiredClientScripts_template.js
@@ -26,7 +26,8 @@ exports.requiredScripts = [
 	'/lib/spin/js/jquery-spin.js',
 	'/lib/handsontable/dist/handsontable.full.js',
 	'/lib/select2-4.0.2/dist/js/select2.full.js',
-	'/socket.io/socket.io.js'
+	'/socket.io/socket.io.js',
+	'/lib/blockUI.js'
 ];
 
 exports.applicationScripts = [
