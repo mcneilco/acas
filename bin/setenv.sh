7cd72d6a2105c55a82a2017f8e3668ee4245e950
Brian Frost <bfrost@mcneilco.com>
2017-11-29 15:40:20 -0800

    Tweaked addition to setenv.sh to only try to source /opt/rh/python27/enable if it exists.

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,3 +1,3 @@
 # Example:
 #export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib:$LD_LIBRARY_PATH
-source /opt/rh/python27/enable
+if [ -e  "/opt/rh/python27/enable" ]; then source /opt/rh/python27/enable; fi;
