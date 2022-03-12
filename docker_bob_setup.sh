f3ecd446e422dc1e6be56e8d16222b4e62c992c3
Brian Bolt <brian.bolt@schrodinger.com>
2022-03-11 17:15:07 -0800

    max timeout

--- docker_bob_setup.sh
+++ docker_bob_setup.sh
@@ -14,11 +14,11 @@ if [ $counter == $wait ]; then
 else
     echo "ACAS started!"
     # getOrCreateACASBob
-    curl -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateACASBob
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateACASBob
     # getOrCreateGlobalProject
-    curl -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateGlobalProject
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateGlobalProject
     # getOrCreateGlobalProjectRole
-    curl -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateGlobalProjectRole
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateGlobalProjectRole
     # giveBobRoles
-    curl -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/giveBobRoles
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/giveBobRoles
 fi
