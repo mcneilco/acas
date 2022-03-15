22113a80f11187793103864670e85da42198ca7d
Brian Bolt <brian.bolt@schrodinger.com>
2022-03-15 16:41:31 -0500

    Run acasclient tests on build Fixes #896

--- docker_bob_setup.sh
+++ docker_bob_setup.sh
@@ -1,7 +1,25 @@
-curl localhost:3001/api/systemTest/getOrCreateACASBob
-curl localhost:3001/api/systemTest/getOrCreateGlobalProject
-curl localhost:3001/api/systemTest/getOrCreateGlobalProjectRole
-curl localhost:3001/api/systemTest/giveBobRoles
-curl localhost:3001/api/systemTest/getOrCreateCmpdRegBob
-curl localhost:3001/api/systemTest/syncRoles
-#curl localhost:3001/api/systemTest/getOrCreateCmpds
+counter=0
+wait=300
+until $(curl --output /dev/null --silent --head --fail http://localhost:3001/api/authors) || [ $counter == $wait ]; do
+    if [ $counter == 0 ]; then
+        echo "Waiting for ACAS API to start..."
+    fi
+    sleep 1
+    remaining=$((wait - counter))
+    echo "Waiting for ACAS API to start...remaining attempts: $remaining"
+    counter=$((counter+1))
+done
+if [ $counter == $wait ]; then
+    echo "waited $wait seconds for acas to start, giving up"
+else
+    echo "ACAS started!"
+    # getOrCreateACASBob
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateACASBob
+    # getOrCreateGlobalProject
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateGlobalProject
+    # getOrCreateGlobalProjectRole
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/getOrCreateGlobalProjectRole
+    # giveBobRoles
+    curl --max-time 5 -i -X GET -H "Accept: application/json" http://localhost:3001/api/systemTest/giveBobRoles
+fi
+docker-compose logs
