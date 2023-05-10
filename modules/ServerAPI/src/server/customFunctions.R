e579442a22214b9857129220f2e18d81b2f540b3
Brian Bolt <brian.bolt@boltengineered.com>
2023-05-10 16:46:45 -0700

    Removing old code

--- modules/ServerAPI/src/server/customFunctions.R
+++ modules/ServerAPI/src/server/customFunctions.R
@@ -85,46 +85,6 @@ customSourceFileMove <- function(sourceLocations, recordedBy, fileNames = NA, en
   return(savedTargetLocations)
 }
 
-moveAllEntityFilesToGCS <- function() {
-
-  racasMessenger$logger$info("Initialize moving all entity files to GCS")
-  # The Google Storage API handles only one file at a time, so for bulk uploads you need to use a loop or an apply function. To test it, we can download two random pdfs.
-  localRelativePath <- racas::applicationSettings$server.file.server.path
-  for(entityType in names(entityFileStorePaths)) {
-    entityFolder <- entityFileStorePaths[entityType]
-    racasMessenger$logger$info(paste0("Moving files for entity type: ", entityType))
-    entityFolderPath <- file.path(racas::applicationSettings$server.file.server.path, entityFolder)
-    
-    entityFiles <- list.files(entityFolderPath, recursive = TRUE)
-    
-    for(entityFile in entityFiles) {
-      racasMessenger$logger$info(paste0("Moving file: ", entityFile))
-      # Get the code name from the first subfolder
-      codeName <- strsplit(entityFile, "/")[[1]][1]
-      # Try and get the entity from the database using the code name
-      # Otherwise, fall back to creting a fake entity so that customSourceFileMove can still work
-      entity <- NULL
-      if(entityType %in% c("experiment", "protocol")) {
-        try(entity <- racas::getEntityByCodeName(codeName, entityFolder, include = NULL), silent = TRUE)
-        if(is.null(entity)) {
-          racasMessenger$logger$info(paste0("Entity not found in database, saving file anyway: ", codeName))
-          entity <- list(codeName = codeName)
-          recrodedBy <- "acas"
-        } else {
-          recordedBy <- entity$recordedBy
-        }
-        recordedBy <- entity$recordedBy
-      } else if (entityType == "cmpdreg_bulkload") {
-        entity <- NA
-        recordedBy <- "acas"
-      }
-      relativePath <- getUploadedFilePath(file.path(entityFolder, entityFile))
-      result <- customSourceFileMove(relativePath, recordedBy, fileNames = NA, entityType = entityType, entity = entity, deleteOldFile = TRUE, additionalPath = "")
-      # Decided to not delete the file from the local server
-    }
-  }
-}
-
 createRawOnlyTreatmentGroupData <- function(subjectData, sigFigs, inputFormat) {
   # Calculates the treatment group data when averaging subject level data
   if(inputFormat == "") {
