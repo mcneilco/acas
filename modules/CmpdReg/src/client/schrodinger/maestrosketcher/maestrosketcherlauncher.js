8840bc3368d7b41866166d1448482120d0c8feec
Brian Bolt <brian.bolt@schrodinger.com>
2024-06-27 11:53:25 -0700

    ACAS-784: Lookup qtLoader.module

--- modules/CmpdReg/src/client/schrodinger/maestrosketcher/maestrosketcherlauncher.js
+++ modules/CmpdReg/src/client/schrodinger/maestrosketcher/maestrosketcherlauncher.js
@@ -2,7 +2,7 @@ $(function() {
     window.MaestroJSUtil = {
         "getSketcher": function getSketcher(el) {
             function waitForReady(resolve, reject) {
-                var maestro = $(el)?.[0]?.contentWindow?.Module;
+                var maestro = $(el)?.[0]?.contentWindow?.qtLoader?.module;
                 const sketcherImportText = maestro?.sketcher_import_text || maestro?.sketcherImportText;
                 if (!sketcherImportText) {
                     setTimeout(() => waitForReady(resolve, reject), 1000);
