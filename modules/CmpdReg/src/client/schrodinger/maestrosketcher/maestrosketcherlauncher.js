b65bfd8096da51fa5751de6a2f861b4ae0ed5959
Brian Bolt <brian.bolt@schrodinger.com>
2024-06-27 11:52:15 -0700

    ACAS-784: Simplify lookup of maestro to use ?

--- modules/CmpdReg/src/client/schrodinger/maestrosketcher/maestrosketcherlauncher.js
+++ modules/CmpdReg/src/client/schrodinger/maestrosketcher/maestrosketcherlauncher.js
@@ -1,23 +1,16 @@
 $(function() {
-
-	window.MaestroJSUtil = {
-        // See implementation of "setRepresentation" in src/components/Sketcher/index.tsx of the rc-maestro-sketcher repository as a reference
-		"getSketcher": function getSketcher (el) {
-            function waitForReady (resolve, reject) {
-                var _a;
-                maestro = $(el)[0].contentWindow.Module;
-                const sketcherImportText = (_a = this.maestro) === null || _a === void 0 ? void 0 : (_a.sketcher_import_text || _a.sketcherImportText);
-                // Wait until sketcher is initialized
+    window.MaestroJSUtil = {
+        "getSketcher": function getSketcher(el) {
+            function waitForReady(resolve, reject) {
+                var maestro = $(el)?.[0]?.contentWindow?.Module;
+                const sketcherImportText = maestro?.sketcher_import_text || maestro?.sketcherImportText;
                 if (!sketcherImportText) {
-                    self = this;
-                    setTimeout(function() {waitForReady(resolve, reject)}, 1000);
-                }
-                else {
-                    resolve(maestro)
+                    setTimeout(() => waitForReady(resolve, reject), 1000);
+                } else {
+                    resolve(maestro);
                 }
             }
-			
-			return new Promise(waitForReady);				
-		}
-	};
+            return new Promise(waitForReady);
+        }
+    };
 });
